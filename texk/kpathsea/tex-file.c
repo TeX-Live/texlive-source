9d7ff6152315508034c98ccc6b9d59fb94cc4e5d
Karl Berry <karl@freefriends.org>
2007-07-06 17:21:06 +0000

    suffix_search_only=true for lua
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@4552 c570f23f-e606-0410-a88d-b1316a301751

--- texk/kpathsea/tex-file.c
+++ texk/kpathsea/tex-file.c
@@ -675,7 +675,7 @@ kpse_init_format P1C(kpse_file_format_type, format)
       INIT_FORMAT ("lua", DEFAULT_LUAINPUTS, LUA_ENVS);
 #define LUA_SUFFIXES ".luc", ".luctex", ".texluc", ".lua", ".luatex", ".texlua"
       SUFFIXES (LUA_SUFFIXES);
-      FMT_INFO.suffix_search_only = false;
+      FMT_INFO.suffix_search_only = true;
       break;
     default:
       FATAL1 ("kpse_init_format: Unknown format %d", format);
