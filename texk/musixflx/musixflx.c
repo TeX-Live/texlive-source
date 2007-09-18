42a7bc6495469afc5766beb4cc92c2b43941da0d
Karl Berry <karl@freefriends.org>
2007-09-18 00:19:25 +0000

    gplv2+ per author
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@4966 c570f23f-e606-0410-a88d-b1316a301751

--- texk/musixflx/musixflx.c
+++ texk/musixflx/musixflx.c
@@ -1,6 +1,22 @@
 #define VERSION "0.83"
 #define MYVERSION "/T.63dt+jh.2\0" /* Revision jh.2  jh-2 */
 
+/* Copyright (C) 1992, 1993, 1994, 1995, 1997, 2007 Ross Mitchell.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+
 /****************************************************************************
  Line breaking program for MusiXTeX.
  Ross Mitchell, August 1992.
