02ad459e0e3aaae5753893a333102c79605d6fdf
Siep Kroonenberg <siepo@cybercomm.nl>
2008-01-01 17:09:47 +0000

    Patch to prevent lf/crlf conversion. According to Lasse Collin, this patch has also been applied in their repository.
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@6020 c570f23f-e606-0410-a88d-b1316a301751

--- utils/lzma-utils/src/lzmadec/lzmadec.c
+++ utils/lzma-utils/src/lzmadec/lzmadec.c
@@ -12,6 +12,12 @@
 #define CHUNKSIZE_IN 4096
 #define CHUNKSIZE_OUT (1024*512)
 
+
+/* for O_BINARY: */
+#ifdef WIN32
+#include <fcntl.h>
+#endif
+
 int main(int argc, char **argv)
 {
 	int ret;
@@ -38,6 +44,12 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
+ /*
+  * Windows: set stdin/stdout to binary mode */
+        #ifdef WIN32
+        setmode(fileno(stdout), O_BINARY);
+        setmode(fileno(stdin), O_BINARY);
+        #endif
 	if (buffer_in == NULL || buffer_out == NULL) {
 		fprintf (stderr, "%s: Not enough memory.\n", argv[0]);
 		return 5;
