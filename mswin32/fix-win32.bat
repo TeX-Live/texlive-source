d856fdc0f1c31066b07ee0fd0116f2a74b57b6d5
Karl Berry <karl@freefriends.org>
2008-01-30 22:53:16 +0000

    remove windows sources no longer used, per akira
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@6454 c570f23f-e606-0410-a88d-b1316a301751

--- mswin32/fix-win32.bat
+++
@@ -1,4 +0,0 @@
-rem @echo off
-rem dir /s/b *.tpm | perl \source\fptex\mswin32\global-replace.pl --line-mode --backup-dir=backup-win32 "{ $a=chr(10); $_=~s@^man/man(.)/([^\.]*)\.(.)@texmf/doc/man/man\1/\2.\3${a}texmf/doc/man/man\1/\2.html@; }"
-dir /s/b *.tpm | perl \source\fptex\mswin32\global-replace.pl --line-mode --backup-dir=backup-win32 "{ $a=chr(10); $_=~s@^${a}@@; }"
-rem popd
--- texk/windvi/version.h
+++
@@ -1,6 +0,0 @@
-#define XDVERSION "Windvi 0.67.3 based on xdvi 22.15-k-t1"
-#ifdef Omega
-#define TVERSION XDVERSION" for Omega"
-#else
-#define	TVERSION XDVERSION
-#endif
