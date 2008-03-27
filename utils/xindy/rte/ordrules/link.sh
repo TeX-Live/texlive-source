ab484e7b51bbabb703bc1a0cd948ae182205dbf8
Vladimir Volovich <vvv@vsu.ru>
2008-03-27 02:08:18 +0000

    rte/ordrules/link.sh: use ${MAKE-make} instead of make, in case user's make program is not called "make"
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@7173 c570f23f-e606-0410-a88d-b1316a301751

--- utils/xindy/rte/ordrules/link.sh
+++ utils/xindy/rte/ordrules/link.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
 file_list=''
 mod_list=''
 if test -r ordrulei.c; then
   file_list="$file_list"' ordrulei.o'
   mod_list="$mod_list"' ordrulei'
 fi
-make clisp-module CC="${CC}" CFLAGS="${CFLAGS}" INCLUDES="$absolute_linkkitdir"
+${MAKE-make} clisp-module CC="${CC}" CFLAGS="${CFLAGS}" INCLUDES="$absolute_linkkitdir"
 NEW_FILES="$file_list ordrules.o rxsub.o"
 NEW_LIBS="$file_list ordrules.o rxsub.o"
 NEW_MODULES="$mod_list"
