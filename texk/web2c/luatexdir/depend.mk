adbd6d50df82e064b5f9775bda28e5c5df6a6d51
Vladimir Volovich <vvv@vsu.ru>
2008-05-26 09:56:58 +0000

    minor fix - make the (part of the) build work not only when the build directory is a sub-directory of the source directory. luatex build is still not working in this case, but this is a small step to solve the problem.
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@8356 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/luatexdir/depend.mk
+++ texk/web2c/luatexdir/depend.mk
@@ -1,10 +1,3 @@
 lua/mp.o: lua/mp.c mplib.h mpmp.h mppsout.h
-lua/psout.o: lua/psout.c ../../../../texk/web2c/luatexdir/avl.h mplib.h \
-  mpmp.h mppsout.h
-lua/lmplib.o: ../../../../texk/web2c/luatexdir/lua/lmplib.c \
-  \
-  \
-  \
-  \
-  \
-  mplib.h mpmp.h mppsout.h
+lua/psout.o: lua/psout.c avl.h mplib.h mpmp.h mppsout.h
+lua/lmplib.o: lua/lmplib.c mplib.h mpmp.h mppsout.h
