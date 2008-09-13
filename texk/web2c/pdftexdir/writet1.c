0c0469ee5142518181eba03fe3d6e3959b7d227d
Han The Thanh <hanthethanh@gmail.com>
2008-09-13 15:30:12 +0000

    patch to fix t1_log by vvv
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@10587 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/pdftexdir/writet1.c
+++ texk/web2c/pdftexdir/writet1.c
@@ -26,7 +26,7 @@ Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 static const char _svn_version[] =
     "$Id$ $URL: http://scm.foundry.supelec.fr/svn/pdftex/branches/stable/source/src/texk/web2c/pdftexdir/writet1.c $";
 
-#define t1_log(s)        tex_printf("%s",s)
+#define t1_log(str)      tex_printf("%s",str)
 #define get_length1()    t1_length1 = t1_offset() - t1_save_offset
 #define get_length2()    t1_length2 = t1_offset() - t1_save_offset
 #define get_length3()    t1_length3 = fixedcontent? t1_offset() - t1_save_offset : 0
