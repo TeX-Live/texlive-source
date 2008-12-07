7a4e64eda1639b735cd554394520c32d3823f71e
Yue Wang <yuleopen@gmail.com>
2008-12-07 06:40:39 +0000

    fix ncurses compilation problem on FreeBSD-CURRENT
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@11541 c570f23f-e606-0410-a88d-b1316a301751

--- libs/ncurses/ncurses/tinfo/lib_baudrate.c
+++ libs/ncurses/ncurses/tinfo/lib_baudrate.c
@@ -39,6 +39,9 @@
 #include <curses.priv.h>
 #include <term.h>		/* cur_term, pad_char */
 #include <termcap.h>		/* ospeed */
+#if defined(__FreeBSD__)
+#include <sys/param.h>
+#endif
 
 /*
  * These systems use similar header files, which define B1200 as 1200, etc.,
@@ -46,7 +49,7 @@
  * of the indices up to B115200 fit nicely in a 'short', allowing us to retain
  * ospeed's type for compatibility.
  */
-#if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+#if (defined(__FreeBSD__) && (__FreeBSD_version < 700000)) || defined(__NetBSD__) || defined(__OpenBSD__)
 #undef B0
 #undef B50
 #undef B75
