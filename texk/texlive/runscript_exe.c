7de50e621ecc29fbab68c8cd74162318564882ef
Piotr Strzelczyk <piotr@eps.gda.pl>
2009-11-01 10:38:20 +0000

    fix copy-paste error, no recompilation required
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@15924 c570f23f-e606-0410-a88d-b1316a301751

--- texk/texlive/runscript_exe.c
+++ texk/texlive/runscript_exe.c
@@ -1,7 +1,7 @@
 // an .exe part of runscript program
 // see runscript_dll.c for more details
 #include <windows.h>
-__declspec(dllimport) int dllrunscript( argc, argv );
+__declspec(dllimport) int dllrunscript( int ac, char *av[] );
 int main( int argc, char *argv[] ) {
   return dllrunscript( argc, argv );
 }
