c95e10cfd3672beee8d68fef7290a0547afebadb
Peter Breitenlohner <peb@mppmu.mpg.de>
2010-03-15 09:17:35 +0000

    lcdf-typetools: minor build system fix
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@17459 c570f23f-e606-0410-a88d-b1316a301751

--- texk/lcdf-typetools/include/efont/otfdata.hh
+++ texk/lcdf-typetools/include/efont/otfdata.hh
@@ -14,19 +14,16 @@
 # include <byteorder.h>
 #elif HAVE_NETINET_IN_H
 # include <netinet/in.h>
-// MinGW32 has <sys/param.h> but also needs <winsock2.h>
-#elif defined(HAVE_SYS_PARAM_H) || defined(WIN32)
-# if defined(HAVE_SYS_PARAM_H)
-#  include <sys/param.h>
+#elif HAVE_SYS_PARAM_H
+# include <sys/param.h>
+#elif !defined(WIN32)
+# error "configury disaster! Report this error to ekohler@gmail.com"
+#endif
+#ifdef WIN32
+# ifdef __MSC_VER
+#  pragma warning (disable: 4290)
 # endif
-# if defined(WIN32)
-#  ifdef __MSC_VER
-#   pragma warning (disable: 4290)
-#  endif
-#  include <winsock2.h>
-# endif
-#else
-# error "configury disaster! Report this error to kohler@icir.org"
+# include <winsock2.h>
 #endif
 class ErrorHandler;
 namespace Efont { namespace OpenType {
