035e6a4b123ae9a177574731ba1395a2f7a6f79e
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2010-05-05 13:07:41 +0000

    output.c: rewrite the previous fix not to change the output format for non-pTeX dvi.
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@18110 c570f23f-e606-0410-a88d-b1316a301751

--- texk/dvipsk/output.c
+++ texk/dvipsk/output.c
@@ -804,7 +804,7 @@ cmdout(const char *s)
            linepos + l >= LINELENGTH) {
       (void)putc('\n', bitfile);
       linepos = 0;
-   } else if (! lastspecial || linepos > 0) {
+   } else if (! lastspecial || (linepos > 0 && jflag)) {
       (void)putc(' ', bitfile);
       linepos++;
    }
