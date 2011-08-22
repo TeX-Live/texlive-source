4457ee66100bb1c5ef7c1d34b1203ec3787711ea
Peter Breitenlohner <peb@mppmu.mpg.de>
2011-08-22 13:45:08 +0000

    xpdf 3.03
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@23646 c570f23f-e606-0410-a88d-b1316a301751

---
+++ libs/xpdf/xpdf-3.03/xpdf/about-text.h
@@ -0,0 +1,24 @@
+//========================================================================
+//
+// about-text.h
+//
+// Copyright 2002-2007 Glyph & Cog, LLC
+//
+//========================================================================
+
+static const char *aboutWinText[] = {
+  "http://www.foolabs.com/xpdf/",
+  "derekn@foolabs.com",
+  " ",
+  "Licensed under the GNU General Public License (GPL) v2 or v3.",
+  "See the 'README' file for details.",
+  " ",
+  "Supports PDF version " supportedPDFVersionStr ".",
+  " ",
+  "The PDF data structures, operators, and specification",
+  "are copyright 1985-2006 Adobe Systems Inc.",
+  " ",
+  "For more information (including key and mouse bindings)",
+  "please read the xpdf(1) man page.",
+  NULL
+};
