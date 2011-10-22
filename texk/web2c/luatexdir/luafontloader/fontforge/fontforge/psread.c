a481a24af6d0068d01ac41e73e7cacb0ad5b3f4c
Karl Berry <karl@freefriends.org>
2011-10-22 23:08:32 +0000

    branch2011: import luatex array fix from trunk r24353
    git-svn-id: svn://tug.org/texlive/branches/branch2011/Build/source@24363 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/luatexdir/luafontloader/fontforge/fontforge/psread.c
+++ texk/web2c/luatexdir/luafontloader/fontforge/fontforge/psread.c
@@ -385,7 +385,7 @@ return;
     wrapper->top = io;
 }
 
-static int ioescapestopped(IO *wrapper, struct psstack *stack, int sp) {
+static int ioescapestopped(IO *wrapper, struct psstack *stack, int sp, const size_t bsize) {
     _IO *io = wrapper->top, *iop;
     int wasstopped;
 
@@ -396,7 +396,7 @@ static int ioescapestopped(IO *wrapper, struct psstack *stack, int sp) {
 	free(io);
 	if ( wasstopped ) {
 	    wrapper->top = iop;
-	    if ( sp<(int)(sizeof(stack)/sizeof(stack[0])) ) {
+	    if ( sp<(int)bsize ) {
 		stack[sp].type = ps_bool;
 		stack[sp++].u.tf = true;
 	    }
@@ -1609,7 +1609,7 @@ static void _InterpretPS(IO *wrapper, EntityChar *ec, RetStack *rs) {
 	    }
 	  break;
 	  case pt_stop:
-	    sp = ioescapestopped(wrapper,stack,sp);
+	    sp = ioescapestopped(wrapper,stack,sp,sizeof(stack)/sizeof(stack[0]));
 	  break;
 	  case pt_load:
 	    if ( sp>=1 && stack[sp-1].type==ps_lit ) {
