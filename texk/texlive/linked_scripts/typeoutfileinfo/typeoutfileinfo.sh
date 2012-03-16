b979c0deeee4734bebab77fa0d60f3b609e5c05c
Karl Berry <karl@freefriends.org>
2012-03-16 23:09:50 +0000

    typeoutfileinfo (16mar12)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@25667 c570f23f-e606-0410-a88d-b1316a301751

--- texk/texlive/linked_scripts/typeoutfileinfo/typeoutfileinfo.sh
+++ texk/texlive/linked_scripts/typeoutfileinfo/typeoutfileinfo.sh
@@ -1,15 +1,15 @@
 %!/usr/bin/env bash
 ##
-## This is `typeoutfileinfo.sh', a minimalistic shell script for Unices.
+## This is `typeoutfileinfo.sh', a minimalist shell script for Unices.
 ## 
 ##     ./typeoutfileinfo.sh [FILENAME].[EXT]
 ##
-## runs latex with the readprov package
+## runs latex with the readprov package:
 ##
 ##     http://ctan.org/pkg/readprov
 ##
-## in order to display [FILENAME].[TXT]'s file info (\listfile entry)
-## using \typeout. This requires that [FILENAME].[TXT] contains a 
+## in order to display [FILENAME].[TXT]'s FILE INFO (\listfile entry)
+## using \typeout. This requires that [FILENAME].[EXT] contains a 
 ## \ProvidesFile, \ProvidesPackage, or \ProvidesClass command.
 (
 cat << EOM
@@ -21,13 +21,27 @@ EOM
 ## Copyright (C) 2012 Uwe Lueck, http://contact-ednotes.sty.de.vu/
 ##
 ## This program may be distributed and/or modified under the
-## conditions of the LaTeX Project Public License, either version 1.2
+## conditions of the LaTeX Project Public License, either version 1.3c
 ## of this license or (at your option) any later version.
 ## The latest version of this license is in
 ##   http://www.latex-project.org/lppl.txt
-## and version 1.2 or later is part of all distributions of LaTeX 
+## and version 1.3c or later is part of all distributions of LaTeX 
 ## version 1999/12/01 or later.
 ##
 ## There is NO WARRANTY.
 ##
-## v0.1 as of 2012-03-13
+## This is package version v0.1a as of 2012-03-16.
+##
+## CREDIT: this work derived from Harald Harders' `latexfileversion' 
+## as a simplification using my `readprov'. I learnt "here document" 
+## from `latexfileversion' -- UL.
+##
+## PURPOSE/BACKGROUND: A package like this, `latexfileversion' or 
+## `ltxfileinfo' when you wonder which version of a source file 
+## (package, document component) is available in some directory -- 
+## I typically test package changes by symbolic links in single 
+## project directories before installing them globally, and then 
+## sometimes don't remember ... when I get an "undefined" error, 
+## I wonder whether I have installed the symbolic link or whether 
+## I just forgot to define this, or whether I lost the most recent 
+## version ...
