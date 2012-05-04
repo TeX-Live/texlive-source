64a59ff6bce4c3a86505c1cd5004e168d0cbb6ab
Peter Breitenlohner <peb@mppmu.mpg.de>
2012-05-04 07:10:16 +0000

    web2c: Update 'Copyright 2012' in output from -version
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@26186 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/lib/printversion.c
+++ texk/web2c/lib/printversion.c
@@ -51,7 +51,7 @@ printversionandexit (const_string banner,
 #endif
 
   if (copyright_holder) {
-    printf ("Copyright 2011 %s.\n", copyright_holder);
+    printf ("Copyright 2012 %s.\n", copyright_holder);
     if (!author)
       author = copyright_holder;
   }
