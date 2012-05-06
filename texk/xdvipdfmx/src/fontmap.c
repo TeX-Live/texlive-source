b4a19385843e9a3590888f8bc80aedd32732cdd4
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2012-05-06 02:59:09 +0000

    allow to read a map file without PS name
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@26226 c570f23f-e606-0410-a88d-b1316a301751

--- texk/xdvipdfmx/src/fontmap.c
+++ texk/xdvipdfmx/src/fontmap.c
@@ -593,13 +593,17 @@ fontmap_parse_mapdef_dps (fontmap_rec *mrec,
   skip_blank(&p, endptr);
 
   /* The first field (after TFM name) must be PostScript name. */
-  if (p < endptr) {
-    q = parse_string_value(&p, endptr);
-    if (q) RELEASE(q);
-    skip_blank(&p, endptr);
-  } else {
-    WARN("Missing a PostScript font name.");
-    return -1;
+  /* However, pdftex.map allows a line without PostScript name. */
+
+  if (*p != '"' && *p != '<') {
+    if (p < endptr) {
+      q = parse_string_value(&p, endptr);
+      if (q) RELEASE(q);
+      skip_blank(&p, endptr);
+    } else {
+      WARN("Missing a PostScript font name.");
+      return -1;
+    }
   }
 
   if (p >= endptr) return 0;
