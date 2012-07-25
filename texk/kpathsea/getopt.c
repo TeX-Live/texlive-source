005aebc9646d22802a0b1eb1f782c1ac57e06718
Peter Breitenlohner <peb@mppmu.mpg.de>
2012-07-25 06:57:34 +0000

    kpathsea: Use __CYGWIN__ in getopt.c __CYGWIN32__ which is deprecated and may be removed (from Ken Brown)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@27151 c570f23f-e606-0410-a88d-b1316a301751

--- texk/kpathsea/getopt.c
+++ texk/kpathsea/getopt.c
@@ -73,13 +73,11 @@
 #endif
 #endif
 
-#if defined (WIN32) && !defined (__CYGWIN32__)
+#if defined (WIN32) && !defined (__CYGWIN__)
 /* It's not Unix, really.  See?  Capital letters.  */
 #include <stdlib.h>
 #include <windows.h>
-#ifdef getpid
 #undef getpid
-#endif
 #define getpid() GetCurrentProcessId()
 #endif
 
