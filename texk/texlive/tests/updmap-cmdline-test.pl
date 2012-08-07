24f26da1558757b922e963bb0d3aa5491d427e6e
Peter Breitenlohner <peb@mppmu.mpg.de>
2012-08-07 06:31:41 +0000

    Move more tetex linked scripts from texmf/ to texmf-dist/
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@27327 c570f23f-e606-0410-a88d-b1316a301751

--- texk/texlive/tests/updmap-cmdline-test.pl
+++ texk/texlive/tests/updmap-cmdline-test.pl
@@ -3,14 +3,14 @@
 # Public domain.  Originally written 2011, Karl Berry.
 # Check that updmap can parse various cmdline invocations.
 
-# srcdir = tetex (in the source tree)
+# srcdir = texlive (in the source tree)
 BEGIN { chomp ($srcdir = $ENV{"srcdir"} || `cd \`dirname $0\`/.. && pwd`); }
 require "$srcdir/../tests/common-test.pl";
 
 exit (&main ());
 
 sub main {
-  my $ret = &test_run ("$srcdir/updmap.pl", "--version");
+  my $ret = &test_run ("$srcdir/linked_scripts/tetex/updmap.pl", "--version");
 
   # we aren't going to fix this in the current updmap.pl.
                        # "foo", "--enable Map"
