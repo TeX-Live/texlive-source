9b9ad7d6e06d8fb11e393478caa4bcf0751e56f8
Peter Breitenlohner <peb@mppmu.mpg.de>
2012-08-10 11:32:19 +0000

    dvipsk: Drop unused definition of M_PI & Co
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@27362 c570f23f-e606-0410-a88d-b1316a301751

--- texk/dvipsk/ptexmac.h
+++ texk/dvipsk/ptexmac.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 1996-2004 Han The Thanh, <thanh@pdftex.org>
+Copyright (c) 1996-2012 Han The Thanh, <thanh@pdftex.org>
 
 This file is part of pdfTeX.
 
@@ -22,13 +22,6 @@ $Id: //depot/Build/source.development/TeX/texk/web2c/pdftexdir/ptexmac.h#18 $
 
 #ifndef PDFTEXMAC
 #define PDFTEXMAC
-
-/* Not all systems define it. */
-#ifndef M_PI
-#define M_PI           3.14159265358979323846   /* pi */
-#define M_PI_2         1.5707963267948966192E0  /*Hex  2^ 0 * 1.921FB54442D18 */
-#define M_PI_4         7.8539816339744830962E-1 /*Hex  2^-1 * 1.921FB54442D18 */
-#endif
 
 #ifdef WIN32
 // Why relying on gmalloc() ???
