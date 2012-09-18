7c7761ecf4569e2ec1c57a9072376545d5f6fc62
Peter Breitenlohner <peb@mppmu.mpg.de>
2012-09-18 17:42:30 +0000

    e-upTeX: Adapt to W32TeX (from Akira)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@27707 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/euptexdir/euptexextra.h
+++ texk/web2c/euptexdir/euptexextra.h
@@ -10,9 +10,9 @@
 
 #define BANNER "This is e-upTeX, Version 3.1415926-" PTEX_VERSION "-" UPTEX_VERSION "-" EPTEX_VERSION "-" ETEX_VERSION
 #define COPYRIGHT_HOLDER "D.E. Knuth"
-#define AUTHOR NULL
+#define AUTHOR "Peter Breitenlohner"
 #define PROGRAM_HELP EUPTEXHELP
-#define BUG_ADDRESS "ptex-staff@ml.asciimw.jp"
+#define BUG_ADDRESS "tex-k@tug.org"
 #define DUMP_VAR TEXformatdefault
 #define DUMP_LENGTH_VAR formatdefaultlength
 #define DUMP_OPTION "fmt"
