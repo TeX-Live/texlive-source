2a387437c1c38e91aa9213247349755a4b08e979
Khaled Hosny <khaledhosny@eglug.org>
2012-09-22 00:44:02 +0000

    Restore accidentally removed license text
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@27771 c570f23f-e606-0410-a88d-b1316a301751

--- texk/xdvipdfmx/src/xbb.c
+++ texk/xdvipdfmx/src/xbb.c
@@ -4,7 +4,7 @@
 
     Copyright (C) 2008-2012 by Jin-Hwan Cho <chofchof@ktug.or.kr>
 
-    This program is free.
+    This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.
