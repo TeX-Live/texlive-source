d8184722d189b1eeaef717f2ebb95d526587851d
Khaled Hosny <khaledhosny@eglug.org>
2013-01-10 10:26:34 +0000

    Simplify
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@28786 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/xetexdir/XeTeX_ext.h
+++ texk/web2c/xetexdir/XeTeX_ext.h
@@ -234,15 +234,9 @@ extern FT_Library gFreeTypeLibrary;
 #ifdef HAVE_STDBOOL_H
 # include <stdbool.h>
 #else
-# ifdef HAVE_KPATHSEA_KPATHSEA_H
 /* boolean is an enum type from kpathsea/types.h loaded in
    kpathsea/kpathsea.h, use it as fallback */
-#  define bool boolean
-# else
-typedef int bool;
-#  define true (bool) 1
-#  define false (bool) 0
-# endif
+# define bool boolean
 #endif
 
 #include "XeTeXLayoutInterface.h"
