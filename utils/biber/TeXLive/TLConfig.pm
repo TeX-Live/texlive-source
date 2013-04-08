9941f799ce20a709b2f0a83677f662d1d2a7fe59
Karl Berry <karl@freefriends.org>
2013-04-08 18:20:13 +0000

    sync
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@29748 c570f23f-e606-0410-a88d-b1316a301751

--- utils/biber/TeXLive/TLConfig.pm
+++ utils/biber/TeXLive/TLConfig.pm
@@ -1,11 +1,11 @@
 # TeXLive::TLConfig.pm - module exporting configuration values
-# Copyright 2007, 2008, 2009, 2010, 2011, 2012 Norbert Preining
+# Copyright 2007-2013 Norbert Preining
 # This file is licensed under the GNU General Public License version 2
 # or any later version.
 
 package TeXLive::TLConfig;
 
-my $svnrev = '$Revision: 27010 $';
+my $svnrev = '$Revision: 29725 $';
 my $_modulerevision;
 if ($svnrev =~ m/: ([0-9]+) /) {
   $_modulerevision = $1;
@@ -53,7 +53,7 @@ BEGIN {
 
 # the year of our release, will be used in the location of the
 # network packages, and in menu names, and other places.
-$ReleaseYear = 2012;
+$ReleaseYear = 2013;
 
 # users can upgrade from this year to the current year; maybe a spread
 # of more than one year will be useful at some point, but not now.
