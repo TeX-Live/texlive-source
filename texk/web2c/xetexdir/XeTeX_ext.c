c2d6c10393a372a8aed2b841d31ace1f97af2fbe
Khaled Hosny <khaledhosny@eglug.org>
2013-05-18 11:01:52 +0000

    Fix selection of multiple alternates
    XeTeX pre-0.9999 started counting from zero, so ssty=0 means first;
    ssty=1 second etc, but new code accounted only for 0 case, now we handle
    other values correctly.
    
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@30549 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/xetexdir/XeTeX_ext.c
+++ texk/web2c/xetexdir/XeTeX_ext.c
@@ -994,10 +994,11 @@ loadOTfont(PlatformFontRef fontRef, XeTeXFont font, Fixed scaled_size, const cha
 				features[nFeatures].tag = tag;
 				features[nFeatures].start = 0;
 				features[nFeatures].end = (unsigned int) -1;
-				if (param == 0)
-					features[nFeatures].value = 1;
-				else
-					features[nFeatures].value = param;
+				// for backward compatibility with pre-0.9999 where feature
+				// indices started from 0
+				if (param >= 0)
+					param++;
+				features[nFeatures].value = param;
 				nFeatures++;
 				goto next_option;
 			}
