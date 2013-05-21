8aa195df24e42541f40a197133c3b55cd47c755b
Peter Breitenlohner <peb@mppmu.mpg.de>
2013-05-21 13:53:17 +0000

    dvisvgm 1.2.2
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@30608 c570f23f-e606-0410-a88d-b1316a301751

---
+++ texk/dvisvgm/dvisvgm-1.2.2/src/PageSize.h
@@ -0,0 +1,48 @@
+/*************************************************************************
+** PageSize.h                                                           **
+**                                                                      **
+** This file is part of dvisvgm -- the DVI to SVG converter             **
+** Copyright (C) 2005-2013 Martin Gieseking <martin.gieseking@uos.de>   **
+**                                                                      **
+** This program is free software; you can redistribute it and/or        **
+** modify it under the terms of the GNU General Public License as       **
+** published by the Free Software Foundation; either version 3 of       **
+** the License, or (at your option) any later version.                  **
+**                                                                      **
+** This program is distributed in the hope that it will be useful, but  **
+** WITHOUT ANY WARRANTY; without even the implied warranty of           **
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         **
+** GNU General Public License for more details.                         **
+**                                                                      **
+** You should have received a copy of the GNU General Public License    **
+** along with this program; if not, see <http://www.gnu.org/licenses/>. **
+*************************************************************************/
+
+#ifndef PAGESIZE_H
+#define PAGESIZE_H
+
+#include "MessageException.h"
+
+struct PageSizeException : public MessageException
+{
+	PageSizeException (const std::string &msg) : MessageException(msg) {}
+};
+
+class PageSize
+{
+   public:
+ 		PageSize (double w=0, double h=0) : _width(w), _height(h) {}
+		PageSize (std::string name);
+		void resize (std:: string name);
+		void resize (double w, double h);
+		double widthInPT () const  {return _width;}
+		double heightInPT () const {return _height;}
+		double widthInMM () const  {return _width*25.4/72.27;}
+		double heightInMM () const {return _height*25.4/72.27;}
+		bool valid () const        {return _width > 0 && _height > 0;}
+
+   private:
+		double _width, _height; // in pt
+};
+
+#endif
