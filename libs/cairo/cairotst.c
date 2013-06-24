95fc44500e341e441c049c6f6545fa4256bf8f02
Peter Breitenlohner <peb@mppmu.mpg.de>
2013-06-24 08:31:55 +0000

    cairo 1.12.14 with proxy build system, no more need for pkg-config
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@30883 c570f23f-e606-0410-a88d-b1316a301751

---
+++ libs/cairo/cairotst.c
@@ -0,0 +1,16 @@
+/* cairotst.c: Basic test for libcairo
+ *
+ * Copyright (C) 2013 Peter Breitenlohner <tex-live@tug.org>
+ * You may freely use, modify and/or distribute this file.
+ */
+
+#include <stdio.h>
+#include <pixman.h>
+#include <cairo.h>
+
+int main (int argc, char **argv)
+{
+  printf ("%s: using pixman %s\n", argv[0], pixman_version_string ());
+  printf ("%s: using cairo %s\n", argv[0], cairo_version_string ());
+  return 0;
+}
