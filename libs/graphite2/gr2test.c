08162ab89eea9ac23ee5fb775056b1c8c9a2ebb7
Peter Breitenlohner <peb@mppmu.mpg.de>
2013-07-06 09:17:39 +0000

    graphite2: Added a simple test case
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@31116 c570f23f-e606-0410-a88d-b1316a301751

---
+++ libs/graphite2/gr2test.c
@@ -0,0 +1,20 @@
+/* gr2test.c: Basic test for libgraphite2
+ *
+ * Copyright (C) 2013 Peter Breitenlohner <tex-live@tug.org>
+ * You may freely use, modify and/or distribute this file.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include <stdio.h>
+#include <graphite2/Font.h>
+
+int main (int argc, char **argv)
+{
+  int nMajor, nMinor, nBugFix;
+
+  gr_engine_version(&nMajor, &nMinor, &nBugFix);
+  printf ("%s: using graphite2 %d.%d.%d\n", argv[0], nMajor, nMinor, nBugFix);
+  return 0;
+}
