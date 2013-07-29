b006c34017cbaf62a94a6c0a178d25f95645e415
Peter Breitenlohner <peb@mppmu.mpg.de>
2013-07-29 13:26:14 +0000

    bison 3.0
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@31304 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/otps/otp-parser.h
+++ texk/web2c/otps/otp-parser.h
@@ -1,19 +1,19 @@
-/* A Bison parser, made by GNU Bison 2.6.2.  */
+/* A Bison parser, made by GNU Bison 3.0.  */
 
 /* Bison interface for Yacc-like parsers in C
-   
-      Copyright (C) 1984, 1989-1990, 2000-2012 Free Software Foundation, Inc.
-   
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-   
+
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-   
+
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
@@ -26,13 +26,13 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-   
+
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-#ifndef YY_____________TEX_LIVE____________TEXK_WEB_C_OTPS_OTP_PARSER_H
-# define YY_____________TEX_LIVE____________TEXK_WEB_C_OTPS_OTP_PARSER_H
-/* Enabling traces.  */
+#ifndef YY_YY__TEX_LIVE_TEXK_WEB_C_OTPS_OTP_PARSER_H_INCLUDED
+# define YY_YY__TEX_LIVE_TEXK_WEB_C_OTPS_OTP_PARSER_H_INCLUDED
+/* Debug traces.  */
 #ifndef YYDEBUG
 # define YYDEBUG 0
 #endif
@@ -40,30 +40,29 @@
 extern int yydebug;
 #endif
 
-/* Tokens.  */
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     NUMBER = 258,
-     ID = 259,
-     STRING = 260,
-     LEFTARROW = 261,
-     RIGHTARROW = 262,
-     INPUT = 263,
-     OUTPUT = 264,
-     ALIASES = 265,
-     STATES = 266,
-     TABLES = 267,
-     EXPRESSIONS = 268,
-     PUSH = 269,
-     POP = 270,
-     DIV = 271,
-     MOD = 272,
-     BEG = 273,
-     END = 274
-   };
+  enum yytokentype
+  {
+    NUMBER = 258,
+    ID = 259,
+    STRING = 260,
+    LEFTARROW = 261,
+    RIGHTARROW = 262,
+    INPUT = 263,
+    OUTPUT = 264,
+    ALIASES = 265,
+    STATES = 266,
+    TABLES = 267,
+    EXPRESSIONS = 268,
+    PUSH = 269,
+    POP = 270,
+    DIV = 271,
+    MOD = 272,
+    BEG = 273,
+    END = 274
+  };
 #endif
 /* Tokens.  */
 #define NUMBER 258
@@ -84,29 +83,16 @@ extern int yydebug;
 #define BEG 273
 #define END 274
 
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef int YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
 
+
 extern YYSTYPE yylval;
 
-#ifdef YYPARSE_PARAM
-#if defined __STDC__ || defined __cplusplus
-int yyparse (void *YYPARSE_PARAM);
-#else
-int yyparse ();
-#endif
-#else /* ! YYPARSE_PARAM */
-#if defined __STDC__ || defined __cplusplus
 int yyparse (void);
-#else
-int yyparse ();
-#endif
-#endif /* ! YYPARSE_PARAM */
 
-#endif /* !YY_____________TEX_LIVE____________TEXK_WEB_C_OTPS_OTP_PARSER_H  */
+#endif /* !YY_YY__TEX_LIVE_TEXK_WEB_C_OTPS_OTP_PARSER_H_INCLUDED  */
