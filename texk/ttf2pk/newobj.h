cf706d15c85837998f5cf8132a333d35e11bb502
Peter Breitenlohner <peb@mppmu.mpg.de>
2013-08-06 14:18:16 +0000

    ttf2pk, ttf2pk2: Use mycalloc() to avoid uninitialized TFM header words
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@31365 c570f23f-e606-0410-a88d-b1316a301751

--- texk/ttf2pk/newobj.h
+++ texk/ttf2pk/newobj.h
@@ -15,15 +15,21 @@
 #include "ttf2tfm.h"
 
 
+#if defined(HAVE_LIBKPATHSEA)
+#include <kpathsea/config.h>
+#define mymalloc xmalloc
+#define mycalloc xcalloc
+#define myrealloc xrealloc
+#else
 #if (defined(MSDOS) && defined(__TURBOC__)) || \
     (defined(OS2) && defined(_MSC_VER))
 #define SMALLMALLOC
 #endif
 
-
 void *mymalloc(size_t len);
-void *mycalloc(size_t len);
+void *mycalloc(size_t nmemb, size_t len);
 void *myrealloc(void *oldp, size_t len);
+#endif
 
 char *get_line(FILE *f);
 Boolean texlive_getline(char **bufferp, FILE *f);
