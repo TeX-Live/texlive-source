f8b36b91d3395cfcbcdc15cd9363fd656dc8cbed
Peter Breitenlohner <peb@mppmu.mpg.de>
2013-09-10 12:28:14 +0000

    mendexk: Bug fix [ptex:00349]
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@31621 c570f23f-e606-0410-a88d-b1316a301751

--- texk/mendexk/fwrite.c
+++ texk/mendexk/fwrite.c
@@ -15,7 +15,7 @@ static int range_check(struct index ind, int count, char *lbuff);
 static void linecheck(char *lbuff, char *tmpbuff);
 static void crcheck(char *lbuff, FILE *fp);
 
-#define TAIL(x) (x+strlen(x))
+#define TAIL(x) ((x)+strlen(x))
 
 /* if we don't have vsnprintf() */
 /* #define vsnprintf(buff,len,format,argptr) vsprintf(buff,format,argptr) */
@@ -385,7 +385,7 @@ static int range_check(struct index ind, int count, char *lbuff)
 					}
 					if (strlen(ind.p[j].enc)>0) {
 						sprintf(tmpbuff,"%s%s%s",encap_prefix,ind.p[j].enc,encap_infix);
-						sprintf(tmpbuff,"%s%s%s",ind.p[j].page,encap_suffix,delim_n);
+						sprintf(TAIL(tmpbuff),"%s%s%s",ind.p[j].page,encap_suffix,delim_n);
 						linecheck(lbuff,tmpbuff);
 					}
 				}
