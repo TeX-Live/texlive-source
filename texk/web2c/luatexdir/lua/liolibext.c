03481ef671103dd3cf9ce89e20dd76e971af7036
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2013-12-08 09:20:05 +0000

    Large files in lua
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@32361 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/luatexdir/lua/liolibext.c
+++ texk/web2c/luatexdir/lua/liolibext.c
@@ -60,9 +60,27 @@ static const char _svn_version[] =
 /* }====================================================== */
 
 
+#if defined(LUA_USE_POSIX)
+
+#define l_fseek(f,o,w)		fseeko(f,o,w)
+#define l_ftell(f)		ftello(f)
+#define l_seeknum		off_t
+
+#elif defined(LUA_WIN) && !defined(_CRTIMP_TYPEINFO) \
+   && defined(_MSC_VER) && (_MSC_VER >= 1400)
+/* Windows (but not DDK) and Visual C++ 2005 or higher */
+
+#define l_fseek(f,o,w)		_fseeki64(f,o,w)
+#define l_ftell(f)		_ftelli64(f)
+#define l_seeknum		__int64
+
+#else
+
 #define l_fseek(f,o,w)		fseek(f,o,w)
 #define l_ftell(f)		ftell(f)
 #define l_seeknum		long
+
+#endif
 
 #define IO_PREFIX	"_IO_"
 #define IO_INPUT	(IO_PREFIX "input")
