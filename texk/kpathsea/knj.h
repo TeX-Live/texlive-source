9e7bb06cccaf09fc59851d3bb1c39f9b915d00ba
Takuji Tanaka <KXD02663@nifty.ne.jp>
2014-01-01 00:27:26 +0000

    Use cast for ASCII string conversion from char to wchar_t (Windows only)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@32530 c570f23f-e606-0410-a88d-b1316a301751

--- texk/kpathsea/knj.h
+++ texk/kpathsea/knj.h
@@ -1,7 +1,7 @@
 /* knj.h: check for 2-Byte Kanji (CP 932, SJIS) codes.
 
    Copyright 2010, 2011 Akira Kakuto.
-   Copyright 2013 TANAKA Takuji.
+   Copyright 2013, 2014 TANAKA Takuji.
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
