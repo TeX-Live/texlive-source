5de1a81ee9767963d17bdb506ec6c16e0cd9c1a0
Peter Breitenlohner <peb@mppmu.mpg.de>
2014-02-11 12:09:12 +0000

    libs/lua52: Add a basic test
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@32939 c570f23f-e606-0410-a88d-b1316a301751

---
+++ libs/lua52/luatest.c
@@ -0,0 +1,15 @@
+/* pixtest.c: Basic test for libpixman
+ *
+ * Copyright (C) 2014 Peter Breitenlohner <tex-live@tug.org>
+ * You may freely use, modify and/or distribute this file.
+ */
+
+#include <stdio.h>
+#include <lua.h>
+
+int main (int argc, char **argv)
+{
+  printf ("%s: Compiled with %s\n",
+          argv[0], LUA_VERSION);
+  return 0;
+}
