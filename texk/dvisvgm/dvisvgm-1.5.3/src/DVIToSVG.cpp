fe7934d8aa29431e5c0adf5307717b6e16ba657d
Peter Breitenlohner <peb@mppmu.mpg.de>
2014-04-13 13:09:23 +0000

    dvisvgm 1.5.3
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@33433 c570f23f-e606-0410-a88d-b1316a301751

---
+++ texk/dvisvgm/dvisvgm-1.5.3/src/DVIToSVG.cpp
@@ -0,0 +1,387 @@
+/*************************************************************************
+** DVIToSVG.cpp                                                         **
+**                                                                      **
+** This file is part of dvisvgm -- the DVI to SVG converter             **
+** Copyright (C) 2005-2014 Martin Gieseking <martin.gieseking@uos.de>   **
+**                                                                      **
+** This program is free software; you can redistribute it and/or        **
+** modify it under the terms of the GNU General Public License as       **
+** published by the Free Software Foundation; either version 3 of       **
+** the License, or (at your option) any later version.                  **
+**                                                                      **
+** This program is distributed in the hope that it will be useful, but  **
+** WITHOUT ANY WARRANTY; without even the implied warranty of           **
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         **
+** GNU General Public License for more details.                         **
+**                                                                      **
+** You should have received a copy of the GNU General Public License    **
+** along with this program; if not, see <http://www.gnu.org/licenses/>. **
+*************************************************************************/
+
+#include <config.h>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <sstream>
+#include "Calculator.h"
+#include "DVIToSVG.h"
+#include "DVIToSVGActions.h"
+#include "Font.h"
+#include "FontManager.h"
+#include "FileFinder.h"
+#include "GlyphTracerMessages.h"
+#include "InputBuffer.h"
+#include "InputReader.h"
+#include "Matrix.h"
+#include "Message.h"
+#include "PageRanges.h"
+#include "PageSize.h"
+#include "SVGOutputBase.h"
+//
+///////////////////////////////////
+// special handlers
+
+#include "BgColorSpecialHandler.h"
+#include "ColorSpecialHandler.h"
+#include "DvisvgmSpecialHandler.h"
+#include "EmSpecialHandler.h"
+#include "PdfSpecialHandler.h"
+#include "HtmlSpecialHandler.h"
+#ifndef HAVE_LIBGS
+	#include "NoPsSpecialHandler.h"
+#endif
+#ifndef DISABLE_GS
+	#include "PsSpecialHandler.h"
+#endif
+#include "TpicSpecialHandler.h"
+
+///////////////////////////////////
+
+#ifdef TARGET_SYSTEM
+	#define VERSION_STR VERSION " (" TARGET_SYSTEM ")"
+#else
+	#define VERSION_STR VERSION
+#endif
+
+
+using namespace std;
+
+
+/** Returns time stamp of current date/time. */
+static string datetime () {
+	time_t t;
+	time(&t);
+	struct tm *tm = localtime(&t);
+	char *timestr = asctime(tm);
+	timestr[24] = 0;  // remove newline
+	return timestr;
+}
+
+class PSHeaderActions : public DVIActions
+{
+	public :
+		PSHeaderActions (DVIToSVG &dvisvg) : _dvisvg(dvisvg) {}
+
+		void special (const std::string &str, double dvi2bp) {
+			// execute PS headers only
+			if (!str.empty() && (str[0] == '!' || str.substr(0, 7) == "header="))
+				_dvisvg.specialManager().process(str, dvi2bp, 0);
+		}
+
+		bool fontProcessingEnabled () const {
+			return false;
+		}
+
+		void endPage (unsigned) {
+			_dvisvg.specialManager().leavePSHeaderSection();
+		}
+
+		BoundingBox& bbox () {return _bbox;}
+
+	private:
+		DVIToSVG &_dvisvg;
+		BoundingBox _bbox;
+};
+
+
+/** 'a': trace all glyphs even if some of them are already cached
+ *  'm': trace missing glyphs, i.e. glyphs not yet cached
+ *   0 : only trace actually required glyphs */
+char DVIToSVG::TRACE_MODE = 0;
+
+
+DVIToSVG::DVIToSVG (istream &is, SVGOutputBase &out) : DVIReader(is), _out(out)
+{
+	replaceActions(new DVIToSVGActions(*this, _svg));
+}
+
+
+DVIToSVG::~DVIToSVG () {
+	delete replaceActions(0);
+}
+
+
+/** Starts the conversion process.
+ *  @param[in] first number of first page to convert
+ *  @param[in] last number of last page to convert
+ *  @param[out] pageinfo (number of converted pages, number of total pages) */
+void DVIToSVG::convert (unsigned first, unsigned last, pair<int,int> *pageinfo) {
+	if (getTotalPages() == 0) {
+		executePreamble();
+		executePostamble();    // collect scaling information
+	}
+	if (first > last)
+		swap(first, last);
+	if (first > getTotalPages()) {
+		ostringstream oss;
+		oss << "file contains only " << getTotalPages() << " page";
+		if (getTotalPages() > 1)
+			oss << 's';
+		throw DVIException(oss.str());
+	}
+	last = min(last, getTotalPages());
+
+#if defined(HAVE_LIBGS) || !defined(DISABLE_GS)
+	// ensure loading of PostScript prologues given at the beginning of the first page
+	// (prologue files are always referenced in first page)
+	PSHeaderActions headerActions(*this);
+	DVIActions *save = replaceActions(&headerActions);
+	try {
+		executePage(1);
+		replaceActions(save);
+	}
+	catch (PSException &e) {
+		replaceActions(save);
+		throw;
+	}
+#endif
+
+	for (unsigned i=first; i <= last; ++i) {
+		executePage(i);
+		embedFonts(_svg.rootNode());
+		_svg.write(_out.getPageStream(getCurrentPageNumber(), getTotalPages()));
+		string fname = _out.filename(i, getTotalPages());
+		Message::mstream(false, Message::MC_PAGE_WRITTEN) << "\npage written to " << (fname.empty() ? "<stdout>" : fname) << '\n';
+		_svg.reset();
+		static_cast<DVIToSVGActions*>(getActions())->reset();
+	}
+	if (pageinfo) {
+		pageinfo->first = last-first+1;
+		pageinfo->second = getTotalPages();
+	}
+}
+
+
+/** Convert DVI pages specified by a page range string.
+ *  @param[in] rangestr string describing the pages to convert
+ *  @param[out] pageinfo (number of converted pages, number of total pages) */
+void DVIToSVG::convert (const string &rangestr, pair<int,int> *pageinfo) {
+	if (getTotalPages() == 0) {
+		executePreamble();
+		executePostamble();    // collect scaling information
+	}
+	PageRanges ranges;
+	if (!ranges.parse(rangestr, getTotalPages()))
+		throw MessageException("invalid page range format");
+	FORALL(ranges, PageRanges::ConstIterator, it)
+		convert(it->first, it->second);
+	if (pageinfo) {
+		pageinfo->first = ranges.pages();
+		pageinfo->second = getTotalPages();
+	}
+}
+
+
+/** This template method is called by parent class DVIReader before
+ *  executing the BOP actions.
+ *  @param[in] pageno physical page number (1 = first page)
+ *  @param[in] c contains information about the page (page number etc.) */
+void DVIToSVG::beginPage (unsigned pageno, Int32 *c) {
+	if (dynamic_cast<DVIToSVGActions*>(getActions())) {
+		Message::mstream().indent(0);
+		Message::mstream(false, Message::MC_PAGE_NUMBER) << "processing page " << pageno;
+		if (pageno != (unsigned)c[0])  // Does page number shown on page differ from physical page number?
+			Message::mstream(false) << " [" << c[0] << ']';
+		Message::mstream().indent(1);
+		_svg.appendToDoc(new XMLCommentNode(" This file was generated by dvisvgm " VERSION_STR " "));
+		_svg.appendToDoc(new XMLCommentNode(" " + datetime() + " "));
+	}
+}
+
+
+/** This template method is called by parent class DVIReader before
+ *  executing the EOP actions. */
+void DVIToSVG::endPage (unsigned pageno) {
+	if (!dynamic_cast<DVIToSVGActions*>(getActions()))
+		return;
+
+	_specialManager.notifyEndPage(pageno);
+	// set bounding box and apply page transformations
+	BoundingBox &bbox = getActions()->bbox();
+	bbox.unlock();
+	Matrix matrix;
+	getPageTransformation(matrix);
+	static_cast<DVIToSVGActions*>(getActions())->setPageMatrix(matrix);
+	if (_bboxString == "min")
+		bbox.transform(matrix);
+	if (string("dvi none min").find(_bboxString) == string::npos) {
+		istringstream iss(_bboxString);
+		StreamInputReader ir(iss);
+		ir.skipSpace();
+		if (isalpha(ir.peek())) {
+			// set explicitly given page format
+			PageSize size(_bboxString);
+			if (size.valid()) {
+				// convention: DVI position (0,0) equals (1in, 1in) relative
+				// to the upper left vertex of the page (see DVI specification)
+				const double border = -72;
+				bbox = BoundingBox(border, border, size.widthInBP()+border, size.heightInBP()+border);
+			}
+		}
+		else { // set/modify bounding box by explicitly given values
+			try {
+				bbox.set(_bboxString);
+			}
+			catch (const MessageException &e) {
+			}
+		}
+	}
+	else if (_bboxString == "dvi") {
+		// center page content
+		double dx = (getPageWidth()-bbox.width())/2;
+		double dy = (getPageHeight()-bbox.height())/2;
+		bbox += BoundingBox(-dx, -dy, dx, dy);
+	}
+	if (_bboxString != "none") {
+		if (bbox.width() == 0)
+			Message::wstream(false) << "\npage is empty\n";
+		else {
+			_svg.setBBox(bbox);
+
+			const double bp2pt = 72.27/72;
+			const double bp2mm = 25.4/72;
+			Message::mstream(false) << '\n';
+			Message::mstream(false, Message::MC_PAGE_SIZE) << "page size: " << XMLString(bbox.width()*bp2pt) << "pt"
+				" x " << XMLString(bbox.height()*bp2pt) << "pt"
+				" (" << XMLString(bbox.width()*bp2mm) << "mm"
+				" x " << XMLString(bbox.height()*bp2mm) << "mm)";
+			Message::mstream(false) << '\n';
+		}
+	}
+}
+
+
+void DVIToSVG::getPageTransformation(Matrix &matrix) const {
+	if (_transCmds.empty())
+		matrix.set(1);  // unity matrix
+	else {
+		Calculator calc;
+		if (getActions()) {
+			const double bp2pt = 72.27/72;
+			BoundingBox &bbox = getActions()->bbox();
+			calc.setVariable("ux", bbox.minX()*bp2pt);
+			calc.setVariable("uy", bbox.minY()*bp2pt);
+			calc.setVariable("w",  bbox.width()*bp2pt);
+			calc.setVariable("h",  bbox.height()*bp2pt);
+		}
+		calc.setVariable("pt", 1);
+		calc.setVariable("in", 72.27);
+		calc.setVariable("cm", 72.27/2.54);
+		calc.setVariable("mm", 72.27/25.4);
+		matrix.set(_transCmds, calc);
+	}
+}
+
+
+static void collect_chars (map<const Font*, set<int> > &fm) {
+	typedef const map<const Font*, set<int> > UsedCharsMap;
+	FORALL(fm, UsedCharsMap::const_iterator, it) {
+		if (it->first->uniqueFont() != it->first) {
+			FORALL(it->second, set<int>::const_iterator, cit)
+				fm[it->first->uniqueFont()].insert(*cit);
+		}
+	}
+}
+
+
+/** Adds the font information to the SVG tree.
+ *  @param[in] svgElement the font nodes are added to this node */
+void DVIToSVG::embedFonts (XMLElementNode *svgElement) {
+	if (!svgElement)
+		return;
+	if (!getActions())  // no dvi actions => no chars written => no fonts to embed
+		return;
+
+	typedef map<const Font*, set<int> > UsedCharsMap;
+	const DVIToSVGActions *svgActions = static_cast<DVIToSVGActions*>(getActions());
+	UsedCharsMap &usedChars = svgActions->getUsedChars();
+
+	collect_chars(usedChars);
+
+	GlyphTracerMessages messages;
+	FORALL(usedChars, UsedCharsMap::const_iterator, it) {
+		const Font *font = it->first;
+		if (const PhysicalFont *ph_font = dynamic_cast<const PhysicalFont*>(font)) {
+			if (TRACE_MODE)
+				ph_font->traceAllGlyphs(TRACE_MODE == 'a', &messages);
+
+			if (font->path())  // does font file exist?
+				_svg.append(*ph_font, it->second, &messages);
+			else
+				Message::wstream(true) << "can't embed font '" << font->name() << "'\n";
+		}
+		else
+			Message::wstream(true) << "can't embed font '" << font->name() << "'\n";
+	}
+	_svg.appendFontStyles(svgActions->getUsedFonts());
+}
+
+
+/** Enables or disables processing of specials. If ignorelist == 0, all
+ *  supported special handlers are loaded. To disable selected sets of specials,
+ *  the corresponding prefixes can be given separated by non alpha-numeric characters,
+ *  e.g. "color, ps, em" or "color: ps em" etc.
+ *  A single "*" in the ignore list disables all specials.
+ *  @param[in] ignorelist list of special prefixes to ignore
+ *  @param[in] pswarning if true, shows warning about disabled PS support
+ *  @return the SpecialManager that handles special statements */
+const SpecialManager* DVIToSVG::setProcessSpecials (const char *ignorelist, bool pswarning) {
+	if (ignorelist && strcmp(ignorelist, "*") == 0) { // ignore all specials?
+		_specialManager.unregisterHandlers();
+	}
+	else {
+		// add special handlers
+		SpecialHandler *handlers[] = {
+			0,                          // placeholder for PsSpecialHandler
+			new BgColorSpecialHandler,  // handles background color special
+			new ColorSpecialHandler,    // handles color specials
+			new DvisvgmSpecialHandler,  // handles raw SVG embeddings
+			new EmSpecialHandler,       // handles emTeX specials
+			new HtmlSpecialHandler,     // handles hyperref specials
+			new PdfSpecialHandler,      // handles pdf specials
+			new TpicSpecialHandler,     // handles tpic specials
+			0
+		};
+		SpecialHandler **p = handlers;
+#if !defined(DISABLE_GS)
+		if (Ghostscript().available())
+			*p = new PsSpecialHandler;
+		else
+#endif
+		{
+#if defined(DISABLE_GS) || !defined(HAVE_LIBGS)
+			*p = new NoPsSpecialHandler; // dummy PS special handler that only prints warning messages
+#endif
+			if (pswarning) {
+#if defined(DISABLE_GS)
+				Message::wstream() << "processing of PostScript specials has been disabled permanently\n";
+#else
+				Message::wstream() << "processing of PostScript specials is disabled (Ghostscript not found)\n";
+#endif
+			}
+		}
+		_specialManager.unregisterHandlers();
+		_specialManager.registerHandlers(p, ignorelist);
+	}
+	return &_specialManager;
+}
---
+++ texk/dvisvgm/dvisvgm-1.5.3/src/XMLNode.cpp
@@ -0,0 +1,344 @@
+/*************************************************************************
+** XMLNode.cpp                                                          **
+**                                                                      **
+** This file is part of dvisvgm -- the DVI to SVG converter             **
+** Copyright (C) 2005-2014 Martin Gieseking <martin.gieseking@uos.de>   **
+**                                                                      **
+** This program is free software; you can redistribute it and/or        **
+** modify it under the terms of the GNU General Public License as       **
+** published by the Free Software Foundation; either version 3 of       **
+** the License, or (at your option) any later version.                  **
+**                                                                      **
+** This program is distributed in the hope that it will be useful, but  **
+** WITHOUT ANY WARRANTY; without even the implied warranty of           **
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         **
+** GNU General Public License for more details.                         **
+**                                                                      **
+** You should have received a copy of the GNU General Public License    **
+** along with this program; if not, see <http://www.gnu.org/licenses/>. **
+*************************************************************************/
+
+#include <config.h>
+#include "macros.h"
+#include "XMLNode.h"
+#include "XMLString.h"
+
+using namespace std;
+
+bool XMLNode::emit (ostream &os, XMLNode *stopNode) {
+	if (this == stopNode)
+		return false;
+	write(os);
+	return true;
+}
+
+
+XMLElementNode::XMLElementNode (const string &n) : _name(n), _emitted(false) {
+}
+
+
+XMLElementNode::XMLElementNode (const XMLElementNode &node)
+	: _name(node._name), _attributes(node._attributes), _emitted(false)
+{
+	FORALL(node._children, ChildList::const_iterator, it)
+		_children.push_back((*it)->clone());
+}
+
+
+XMLElementNode::~XMLElementNode () {
+	FORALL(_children, ChildList::iterator, i)
+		delete *i;
+}
+
+
+void XMLElementNode::addAttribute (const string &name, const string &value) {
+	_attributes[name] = value;
+}
+
+
+void XMLElementNode::addAttribute (const string &name, double value) {
+	_attributes[name] = XMLString(value);
+}
+
+
+void XMLElementNode::append (XMLNode *child) {
+	if (!child)
+		return;
+	XMLTextNode *textNode1 = dynamic_cast<XMLTextNode*>(child);
+	if (!textNode1 || _children.empty())
+		_children.push_back(child);
+	else {
+		if (XMLTextNode *textNode2 = dynamic_cast<XMLTextNode*>(_children.back()))
+			textNode2->append(textNode1);  // merge two consecutive text nodes
+		else
+			_children.push_back(child);
+	}
+}
+
+
+void XMLElementNode::append (const string &str) {
+	if (_children.empty() || !dynamic_cast<XMLTextNode*>(_children.back()))
+		_children.push_back(new XMLTextNode(str));
+	else
+		static_cast<XMLTextNode*>(_children.back())->append(str);
+}
+
+
+void XMLElementNode::prepend (XMLNode *child) {
+	if (!child)
+		return;
+	XMLTextNode *textNode1 = dynamic_cast<XMLTextNode*>(child);
+	if (!textNode1 || _children.empty())
+		_children.push_front(child);
+	else {
+		if (XMLTextNode *textNode2 = dynamic_cast<XMLTextNode*>(_children.back()))
+			textNode2->prepend(textNode1);  // merge two consecutive text nodes
+		else
+			_children.push_front(child);
+	}
+}
+
+
+/** Inserts a new child node before a given child node already present. The latter
+ *  will be the following sibling of the node to be inserted. If there's no such
+ *  node present, nothing is inserted.
+ *  @param[in] child node to be inserted
+ *  @param[in] sibling following sibling of 'child'
+ *  @return true on success */
+bool XMLElementNode::insertBefore (XMLNode *child, XMLNode *sibling) {
+	ChildList::iterator it = _children.begin();
+	while (it != _children.end() && *it != sibling)
+		++it;
+	if (it != _children.end()) {
+		_children.insert(it, child);
+		return true;
+	}
+	return false;
+}
+
+
+/** Inserts a new child node after a given child node already present. The latter
+ *  will be the preceding sibling of the node to be inserted. If there's no such
+ *  node present, nothing is inserted.
+ *  @param[in] child node to be inserted
+ *  @param[in] sibling preceding sibling of 'child'
+ *  @return true on success */
+bool XMLElementNode::insertAfter (XMLNode *child, XMLNode *sibling) {
+	ChildList::iterator it = _children.begin();
+	while (it != _children.end() && *it != sibling)
+		++it;
+	if (it != _children.end()) {
+		_children.insert(++it, child);
+		return true;
+	}
+	return false;
+}
+
+
+/** Finds all descendant elements of a given name and given attribute.
+ *  @param[in] name name of elements to find
+ *  @param[in] attr_name name of attribute to find
+ *  @param[out] descendants all elements found
+ *  @return true if at least one element was found  */
+bool XMLElementNode::findDescendants (const char *name, const char *attr_name, vector<XMLElementNode*> &descendants) {
+	FORALL(_children, ChildList::iterator, it) {
+		if (XMLElementNode *elem = dynamic_cast<XMLElementNode*>(*it)) {
+			if ((!name || elem->getName() == name) && (!attr_name || elem->hasAttribute(attr_name)))
+				descendants.push_back(elem);
+			elem->findDescendants(name, attr_name, descendants);
+		}
+	}
+	return !descendants.empty();
+}
+
+
+ostream& XMLElementNode::write (ostream &os) const {
+	os << '<' << _name;
+	FORALL(_attributes, AttribMap::const_iterator, i)
+		os << ' ' << i->first << "='" << i->second << '\'';
+	if (_children.empty())
+		os << "/>\n";
+	else {
+		os << '>';
+		if (dynamic_cast<XMLElementNode*>(_children.front()))
+			os << '\n';
+		FORALL(_children, ChildList::const_iterator, i)
+			(*i)->write(os);
+		os << "</" << _name << ">\n";
+	}
+	return os;
+}
+
+
+/** Writes a part of the XML tree to the given output stream and removes
+ *  the completely written nodes. The output stops when a stop node is reached
+ *  (this node won't be printed at all). If a node was only partly emitted, i.e.
+ *  its child was the stop node, a further call of emit will continue the output.
+ *  @param[in] os stream to which the output is sent to
+ *  @param[in] stopNode node where emitting stops (if 0 the whole tree will be emitted)
+ *  @return true if node was emitted completely */
+bool XMLElementNode::emit (ostream &os, XMLNode *stopNode) {
+	if (this == stopNode)
+		return false;
+
+	if (!_emitted) {
+		os << '<' << _name;
+		FORALL(_attributes, AttribMap::iterator, i)
+			os << ' ' << i->first << "='" << i->second << '\'';
+		if (_children.empty())
+			os << "/>\n";
+		else {
+			os << '>';
+			if (dynamic_cast<XMLElementNode*>(_children.front()))
+				os << '\n';
+		}
+
+		_emitted = true;
+	}
+	if (!_children.empty()) {
+		FORALL(_children, ChildList::iterator, i) {
+			if ((*i)->emit(os, stopNode)) {
+				ChildList::iterator it = i++;  // prevent i from being invalidated...
+				_children.erase(it);              // ... by erase
+				--i;  // @@ what happens if i points to first child?
+			}
+			else
+				return false;
+		}
+		os << "</" << _name << ">\n";
+	}
+	return true;
+}
+
+
+/** Returns true if this element has an attribute of given name. */
+bool XMLElementNode::hasAttribute (const string &name) const {
+	return _attributes.find(name) != _attributes.end();
+}
+
+
+/** Returns the value of an attribute.
+ *  @param[in] name name of attribute
+ *  @return attribute value or 0 if attribute doesn't exist */
+const char* XMLElementNode::getAttributeValue(const std::string& name) const {
+	AttribMap::const_iterator it = _attributes.find(name);
+	if (it != _attributes.end())
+		return it->second.c_str();
+	return 0;
+}
+
+
+//////////////////////
+
+void XMLTextNode::append (XMLNode *node) {
+	if (XMLTextNode *tn = dynamic_cast<XMLTextNode*>(node))
+		append(tn);
+	else
+		delete node;
+}
+
+
+void XMLTextNode::append (XMLTextNode *node) {
+	if (node)
+		_text += node->_text;
+	delete node;
+}
+
+
+void XMLTextNode::append (const string &str) {
+	_text += str;
+}
+
+
+void XMLTextNode::prepend (XMLNode *node) {
+	if (XMLTextNode *tn = dynamic_cast<XMLTextNode*>(node))
+		prepend(tn);
+	else
+		delete node;
+}
+
+
+//////////////////////
+
+XMLDeclarationNode::XMLDeclarationNode (const string &n, const string &p)
+	: _name(n), _params(p), _emitted(false)
+{
+}
+
+
+XMLDeclarationNode::XMLDeclarationNode (const XMLDeclarationNode &node)
+	: _name(node._name), _params(node._params), _emitted(false)
+{
+	FORALL(node._children, list<XMLDeclarationNode*>::const_iterator, it)
+		_children.push_back(new XMLDeclarationNode(**it));
+}
+
+
+XMLDeclarationNode::~XMLDeclarationNode () {
+	FORALL(_children, list<XMLDeclarationNode*>::iterator, i)
+		delete *i;
+}
+
+
+/** Appends another declaration node to this one.
+ *  @param[in] child child to append, must be of type XMLDeclarationNode */
+void XMLDeclarationNode::append (XMLNode *child) {
+	if (XMLDeclarationNode *decl_node = dynamic_cast<XMLDeclarationNode*>(child))
+		_children.push_back(decl_node);
+	else
+		delete child;
+}
+
+
+ostream& XMLDeclarationNode::write (ostream &os) const {
+	os << "<!" << _name << ' ' << _params;
+	if (_children.empty())
+		os << ">\n";
+	else {
+		os << "[\n";
+		FORALL(_children, list<XMLDeclarationNode*>::const_iterator, i)
+			(*i)->write(os);
+		os << "]>\n";
+	}
+	return os;
+}
+
+
+bool XMLDeclarationNode::emit (ostream &os, XMLNode *stopNode) {
+	if (this == stopNode)
+		return false;
+
+	if (!_emitted) {
+		os << "<!" << _name << ' ' << _params;
+		if (_children.empty())
+			os << ">\n";
+		else
+			os << "[\n";
+		_emitted = true;
+	}
+	if (!_children.empty()) {
+		FORALL(_children, list<XMLDeclarationNode*>::iterator, i) {
+			if ((*i)->emit(os, stopNode)) {
+				list<XMLDeclarationNode*>::iterator it = i++;  // prevent i from being invalidated...
+				_children.erase(it);              // ... by erase
+				--i;  // @@ what happens if i points to first child?
+			}
+			else
+				return false;
+		}
+		os << "]>\n";
+	}
+	return true;
+
+}
+
+//////////////////////
+
+ostream& XMLCDataNode::write (ostream &os) const {
+	os << "<![CDATA[\n"
+		<< _data
+		<< "]]>\n";
+	return os;
+}
+
+
