5a743f195647d8c5290496fcb93200ac31074bcf
Karl Berry <karl@freefriends.org>
2014-05-19 22:17:16 +0000

    luaotfload (19may14)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@34131 c570f23f-e606-0410-a88d-b1316a301751

--- texk/texlive/linked_scripts/luaotfload/luaotfload-tool.lua
+++ texk/texlive/linked_scripts/luaotfload/luaotfload-tool.lua
@@ -4,12 +4,12 @@
 --  DESCRIPTION:  database functionality
 -- REQUIREMENTS:  luaotfload 2.4
 --       AUTHOR:  Khaled Hosny, Élie Roux, Philipp Gesang
---      VERSION:  2.4-3
+--      VERSION:  2.4-4
 --      LICENSE:  GPL v2
 --     MODIFIED:  2013-07-28 13:12:04+0200
 -----------------------------------------------------------------------
 
-local version = "2.4-3" --- <int: major>.<int: minor><alpha: fixes>
+local version = "2.4-4" --- <int: major>.<int: minor><alpha: fixes>
 
 --[[doc--
 
