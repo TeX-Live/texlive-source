d7603ba5aa2fc7bff4e7768c95005e19ce7592ea
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2014-06-20 02:36:18 +0000

    Fix a potential bug (w32 only)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@34327 c570f23f-e606-0410-a88d-b1316a301751

--- texk/kpathsea/win32lib.c
+++ texk/kpathsea/win32lib.c
@@ -245,15 +245,25 @@ drive name
       }
       if(*p == '\\') {
          *p = '\0';
-         if((hnd = FindFirstFile(q, &ffd)) == INVALID_HANDLE_VALUE) {
-            return 0;
+         if((*(p-2) == '\\' || p-1 == q) && *(p-1) == '.') {
+            cnt += 2;
+            if(cnt > len) return 0;
+            strcat(buff, "./");
+         } else if((*(p-3) == '\\' || p-2 == q) && *(p-2) == '.' && *(p-1) == '.') {
+            cnt += 3;
+            if(cnt > len) return 0;
+            strcat(buff, "../");
+         } else {
+            if((hnd = FindFirstFile(q, &ffd)) == INVALID_HANDLE_VALUE) {
+               return 0;
+            }
+            FindClose(hnd);
+            cnt += strlen(ffd.cFileName);
+            cnt++;
+            if(cnt > len) return 0;
+            strcat(buff, ffd.cFileName);
+            strcat(buff, "/");
          }
-         FindClose(hnd);
-         cnt += strlen(ffd.cFileName);
-         cnt++;
-         if(cnt > len) return 0;
-         strcat(buff, ffd.cFileName);
-         strcat(buff, "/");
          *p = '\\';
       }
    }
