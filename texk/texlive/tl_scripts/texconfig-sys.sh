f121efce2b74374cba7bb175c6b3248abd1f6eea
Karl Berry <karl@freefriends.org>
2014-07-13 00:06:11 +0000

    prepend directory of executable to PATH
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@34586 c570f23f-e606-0410-a88d-b1316a301751

--- texk/texlive/tl_scripts/texconfig-sys.sh
+++ texk/texlive/tl_scripts/texconfig-sys.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # $Id$
-
 # texconfig-sys: Thomas Esser, public domain.
 
 # wrapper script for texconfig with TEXMFVAR and TEXMFCONFIG set to
@@ -16,10 +15,13 @@ test -f /bin/bsh && test -z "$RUNNING_BSH" \
   && { RUNNING_BSH=true; export RUNNING_BSH; exec /bin/bsh $0 ${1+"$@"}; }
 unset RUNNING_BSH
 
-export PATH
-
 # hack around a bug in zsh:
 test -n "${ZSH_VERSION+set}" && alias -g '${1+"$@"}'='"$@"'
+
+# preferentially use subprograms from our own directory.
+mydir=`echo "$0" | sed 's,/[^/]*$,,'`
+mydir=`cd "$mydir" && pwd`
+PATH="$mydir:$PATH"; export PATH
 
 v=`kpsewhich -var-value TEXMFSYSVAR`
 c=`kpsewhich -var-value TEXMFSYSCONFIG`
