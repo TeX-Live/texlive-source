26f184a3a792697d9f7e369ddea52e78291339ed
Peter Breitenlohner <peb@mppmu.mpg.de>
2014-07-18 09:48:56 +0000

    TECkit 2.5.4 (svn r139)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@34651 c570f23f-e606-0410-a88d-b1316a301751

--- libs/teckit/TECkit-2.5.3/source/UpdateUnidata.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Update the Unicode data that TECKit uses
-
-wget -N http://www.unicode.org/Public/UNIDATA/{CompositionExclusions,UnicodeData}.txt
-
-perl MakeNormData.pl >NormalizationData.c
-perl MakeUnicodeNames.pl >UnicodeNames.cpp
