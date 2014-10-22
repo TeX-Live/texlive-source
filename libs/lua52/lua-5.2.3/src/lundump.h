966ec0e3b242ce64e699c48ec561892e6f0d5437
Peter Breitenlohner <peb@mppmu.mpg.de>
2014-10-22 13:01:50 +0000

    Build lua52 as (possibly shared) Libtool library
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@35423 c570f23f-e606-0410-a88d-b1316a301751

--- libs/lua52/lua-5.2.3/src/lundump.h
+++ libs/lua52/lua-5.2.3/src/lundump.h
@@ -17,7 +17,7 @@ LUAI_FUNC Closure* luaU_undump (lua_State* L, ZIO* Z, Mbuffer* buff, const char*
 LUAI_FUNC void luaU_header (lu_byte* h);
 
 /* dump one chunk; from ldump.c */
-LUAI_FUNC int luaU_dump (lua_State* L, const Proto* f, lua_Writer w, void* data, int strip);
+LUA_API int luaU_dump (lua_State* L, const Proto* f, lua_Writer w, void* data, int strip);
 
 /* data to catch conversion errors */
 #define LUAC_TAIL		"\x19\x93\r\n\x1a\n"
