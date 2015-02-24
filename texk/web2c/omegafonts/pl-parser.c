6fda7235ba263cf36ba59c633b1082a0c7bbf39d
Peter Breitenlohner <peb@mppmu.mpg.de>
2015-02-24 10:24:39 +0000

    bison 3.0.4
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@36366 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/omegafonts/pl-parser.c
+++ texk/web2c/omegafonts/pl-parser.c
@@ -1,4 +1,4 @@
-/* A Bison parser, made by GNU Bison 3.0.3.  */
+/* A Bison parser, made by GNU Bison 3.0.4.  */
 
 /* Bison implementation for Yacc-like parsers in C
 
@@ -44,7 +44,7 @@
 #define YYBISON 1
 
 /* Bison version.  */
-#define YYBISON_VERSION "3.0.3"
+#define YYBISON_VERSION "3.0.4"
 
 /* Skeleton name.  */
 #define YYSKELETON_NAME "yacc.c"
