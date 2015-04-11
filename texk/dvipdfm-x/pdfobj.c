ac02eee95639493ca310d3018ac5f5da8cc810d4
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2015-04-11 22:11:10 +0000

    dvipdfm-x: Support PNG gAMA chunk. (S. Hirata)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@36766 c570f23f-e606-0410-a88d-b1316a301751

--- texk/dvipdfm-x/pdfobj.c
+++ texk/dvipdfm-x/pdfobj.c
@@ -2957,8 +2957,11 @@ parse_xrefstm_subsec (pdf_file *pf,
     type = (unsigned char) parse_xrefstm_field(p, W[0], 1);
     if (type > 2)
       WARN("Unknown cross-reference stream entry type.");
+#if 0
+    /* Not sure */
     else if (!W[1] || (type != 1 && !W[2]))
       return -1;
+#endif
 
     field2 = (unsigned long)  parse_xrefstm_field(p, W[1], 0);
     field3 = (unsigned short) parse_xrefstm_field(p, W[2], 0);
