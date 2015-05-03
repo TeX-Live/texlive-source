b2f9e6a02f0cbec94e47ced662e0a000d4e5e901
Karl Berry <karl@freefriends.org>
2015-05-03 18:01:18 +0000

    update doc for --help-formats, clua/bltxml/ris, etc.
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@37163 c570f23f-e606-0410-a88d-b1316a301751

--- texk/kpathsea/kpsewhich.c
+++ texk/kpathsea/kpsewhich.c
@@ -1,7 +1,7 @@
 /* kpsewhich -- standalone path lookup and variable expansion for Kpathsea.
    Ideas from Thomas Esser, Pierre MacKay, and many others.
 
-   Copyright 1995-2014 Karl Berry & Olaf Weber.
+   Copyright 1995-2015 Karl Berry & Olaf Weber.
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -630,7 +630,7 @@ read_command_line (kpathsea kpse, int argc, string *argv)
 
     } else if (ARGUMENT_IS ("version")) {
       puts (kpathsea_version_string);
-      puts ("Copyright 2014 Karl Berry & Olaf Weber.\n\
+      puts ("Copyright 2015 Karl Berry & Olaf Weber.\n\
 License LGPLv2.1+: GNU Lesser GPL version 2.1 or later <http://gnu.org/licenses/lgpl.html>\n\
 This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n");
