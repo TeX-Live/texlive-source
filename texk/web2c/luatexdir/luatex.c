b9afe146ec0d7e712c9f420fd2dfcb0c6cd3b92a
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2015-05-06 10:14:56 +0000

    luatexdir: Sync with the upstream.
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@37229 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/luatexdir/luatex.c
+++ texk/web2c/luatexdir/luatex.c
@@ -26,7 +26,7 @@
 
 #define TeX
 
-int luatex_svn = 5228;
+int luatex_svn = 5231;
 int luatex_version = 80;        /* \.{\\luatexversion}  */
 int luatex_revision = '0';      /* \.{\\luatexrevision}  */
 int luatex_date_info = 2015042200;     /* the compile date is now hardwired */
