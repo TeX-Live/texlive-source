0a3e19bec22b8769dace9fa8ebd84f17c9696575
Peter Breitenlohner <peb@mppmu.mpg.de>
2015-06-18 10:49:17 +0000

    Sync WIN32 versions of mktex* with W32TeX
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@37597 c570f23f-e606-0410-a88d-b1316a301751

--- texk/kpathsea/win32/mktexlsr.c
+++ texk/kpathsea/win32/mktexlsr.c
@@ -1,6 +1,21 @@
-/*
-mktexlsr
-Web2C 2010 (2010/08/22 --ak)
+/* mktexlsr.c
+
+   Copyright 2000, 2015 Akira Kakuto.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this library; if not, see <http://www.gnu.org/licenses/>.
+
+   Web2C 2010 (2010/08/22 --ak)
 */
 
 #include <kpathsea/kpathsea.h>
@@ -11,8 +26,6 @@ Web2C 2010 (2010/08/22 --ak)
 #include <kpathsea/dirent.h>
 #endif
 
-#define  MAXTREE  32 /* Max number of texmf trees */
-
 /* magic header */
 #define HDL "%% ls-R -- filename database for kpathsea; do not change this line.\n"
 
@@ -21,16 +34,24 @@ Web2C 2010 (2010/08/22 --ak)
 FILE *ls_R;
 
 static void
-search(const char *name)
+search(char *name)
 {
   DIR *dp;
   struct dirent *de;
   char   buff[256];
+  char   *p;
   int    len;
 
-  if ((dp = opendir(name))) {
+  for(p = name; *p; p++) {
+    if(IS_KANJI(p))
+      p++;
+    else if (*p == '\\')
+      *p = '/';
+  }
+
+  if((dp = opendir(name))) {
     fprintf(ls_R, "\n%s:\n", name);
-    while ((de = readdir(dp))) {
+    while((de = readdir(dp))) {
       if(stricmp(de->d_name, ".") &&
          stricmp(de->d_name, "..") &&
          stricmp(de->d_name, ".bzr") &&
@@ -47,13 +68,15 @@ search(const char *name)
     }
     closedir(dp);
 
-    len = strlen (name);
-    strcpy (buff, name);
-    if (name[len-1] != '/')
-      buff[len++] = '/';
+    len = (int)strlen(name);
+    strcpy(buff, name);
+    if(name[len-1] != '/') {
+      strcat(buff, "/");
+      len++;
+    }
 
     dp = opendir(name);
-    while ((de = readdir(dp))) {
+    while((de = readdir(dp))) {
       if(stricmp(de->d_name, ".") &&
          stricmp(de->d_name, "..") &&
          stricmp(de->d_name, ".bzr") &&
@@ -67,15 +90,14 @@ search(const char *name)
          stricmp(de->d_name, "_darcs")) {
 #ifdef __MINGW32__
         strcpy(buff + len, de->d_name);
-        if (is_dir (buff)) {
-          search (buff);
-        }
+        if(!is_dir(buff))
+          continue;
 #else
-        if (de->d_isdir) {
-          strcpy (buff + len, de->d_name);
-          search (buff);
-        }
-#endif
+        if(!de->d_isdir)
+          continue;
+        strcpy(buff + len, de->d_name);
+#endif          
+        search(buff);
       }
     }
     closedir(dp);
@@ -84,33 +106,25 @@ search(const char *name)
 
 int Quiet = 0;
 
+char first_name[] = "./";
+
 int main(int ac, char **av)
 {
-  char *texmfdbs ;
+  char *texmfdbs = NULL;
   int cdrive, tdrive;
-  char ls_R_name[256];
-  int len, i, numtree;
+  char ls_R_name[512];
+  int i, numtree;
+  size_t len;
   char *progname;
-  char *pathbuff[MAXTREE];
-  char *p, *q;
+  char **pathbuff = NULL;
+  char *p, *pa, *pc;
 
-  progname = av[0];
-  kpse_set_program_name(progname, NULL);
-
-  for(i = 0; i < MAXTREE; i++) {
-    pathbuff[i] = (char *)malloc(384);
-    if(!pathbuff[i]) {
-      fprintf(stderr, "Memory allocation error.\n");
-      exit(100);
-    }
-  }
+  kpse_set_program_name(av[0], NULL);
+  progname = kpse_program_name;
 
   if(ac > 1 && (!strncmp(av[1], "-v", 2) || !strncmp(av[1], "--v", 3))) {
     puts (kpathsea_version_string);
-    puts ("Copyright 2010 Karl Berry & Olaf Weber.\n\
-There is NO warranty.  You may redistribute this software\n\
-under the terms of the GNU General Public License.\n\
-For more information about these matters, see the files named GPL and LGPL.");
+    puts ("mktexlsr: (C version 1.1 --ak 2002-2015)");
     exit (0);
   }
 
@@ -131,21 +145,17 @@ For more information about these matters, see the files named GPL and LGPL.");
       av++;
     }
   }
+
   if(ac > 1) {
-    if(ac > MAXTREE + 1) {
-      fprintf(stderr, "Number of trees must be less than %d.\n"
-	      "Some trees are discarded.\n", MAXTREE);
-      numtree = MAXTREE;
-    } else {
-      numtree = ac - 1;
-    }
+    numtree = ac - 1;
+    pathbuff = xmalloc(numtree * sizeof(char *));
     for(i=0; i < numtree; i++) {
-      strcpy(pathbuff[i], av[i+1]);
-      for(p=pathbuff[i]; *p; p++) {
-	if(*p == '\\')
-	  *p = '/';
-        else if (IS_KANJI(p))
-	  p++;
+      pathbuff[i] = xstrdup(av[i+1]);
+      for(p = pathbuff[i]; *p; p++) {
+        if(IS_KANJI(p))
+          p++;
+        else if(*p == '\\')
+          *p = '/';
       }
     }
   } else {
@@ -157,30 +167,61 @@ For more information about these matters, see the files named GPL and LGPL.");
     texmfdbs = kpse_brace_expand(p);
     free(p);
 
-    for(i=0 ; (i < MAXTREE) && *texmfdbs ; i++) {
-      p = strchr(texmfdbs, ';');
-      if(!p) {
-	strcpy(pathbuff[i], texmfdbs);
-	q = pathbuff[i];
-	if(*q == '!' && *(q+1) == '!')
-	  q += 2;
-	strcpy(pathbuff[i], q);
-	i++;
-	break;
+    p = texmfdbs;
+    i = 0;
+
+    while(*p) {
+      if(*p == '!' && *(p+1) == '!')
+        p += 2;
+      if(*p == ';')
+        while(*p == ';')
+          p++;
+      if(*p && *p != '!') {
+        while(*p != ';' && *p)
+          p++;
+        if(*p == ';') {
+          i++;
+          while(*p == ';')
+            p++;
+        } else if(*p == '\0') {
+          i++;
+          break;
+        }
       }
-      *p = '\0';
-      strcpy(pathbuff[i], texmfdbs);
-      q = pathbuff[i];
-      if(*q == '!' && *(q+1) == '!')
-	q += 2;
-      strcpy(pathbuff[i], q);
-      p++;
-      texmfdbs = p;
     }
 
     numtree = i;
+
+    pathbuff = malloc(numtree * sizeof(char *));
+
+    i = 0;
+    pa = texmfdbs;
+
+    while (*pa) {
+      if (*pa == '!' && *(pa + 1) == '!')
+        pa += 2;
+      if (*pa == ';') {
+        while (*pa == ';')
+          pa++;
+      }
+      if(*pa && *pa != '!') {
+        pathbuff[i] = malloc(strlen(pa) + 1);
+        pc = pathbuff[i];
+        while (*pa != ';' && *pa)
+          *pc++ = *pa++;
+        *pc = '\0';
+        if (*pa == ';') {
+          while (*pa == ';')
+            pa++;
+          i++;
+        } else if (*pa == '\0') {
+          i++;
+          break;
+        }
+      }
+    }
   }
-  
+
   for(i = 0; i < numtree; i++) {
     strcpy(ls_R_name, pathbuff[i]);
     len = strlen(ls_R_name);
@@ -189,9 +230,9 @@ For more information about these matters, see the files named GPL and LGPL.");
 
     ls_R = fopen(ls_R_name, "wb");
     if(!ls_R) {
-      fprintf(stderr, "Cannot open %s to write.\n", ls_R_name);
+      fprintf(stdout, "Cannot open %s to write.\n", ls_R_name);
       if(!stricmp(ls_R_name, VARTEXFONTS))
-        fprintf(stderr, "       (Don't mind this message.)\n");
+        fprintf(stdout, "       (Don't mind this message.)\n");
       continue;
     }
 
@@ -204,22 +245,27 @@ For more information about these matters, see the files named GPL and LGPL.");
 
     _chdir(pathbuff[i]);
     if(!Quiet)
-      fprintf(stderr, "%s: Updating %s...\n", progname, ls_R_name);
+      fprintf(stdout, "%s: Updating %s...\n", progname, ls_R_name);
     fprintf(ls_R, HDL);
-    search("./");
+    search(first_name);
 
     fclose(ls_R);
     if(!Quiet)
-      fprintf(stderr, "%s: Updated %s.\n", progname, ls_R_name);
+      fprintf(stdout, "%s: Updated %s.\n", progname, ls_R_name);
     _chdrive(cdrive);
   }
 
   if(!Quiet)
-    fprintf(stderr, "%s: Done.\n", progname);
+    fprintf(stdout, "%s: Done.\n", progname);
   
-  for(i = 0; i < MAXTREE; i++) {
+  for(i = 0; i < numtree; i++) {
     free(pathbuff[i]);
   }
+
+  free (pathbuff);
+
+  if(texmfdbs)
+    free(texmfdbs);
 
   return 0;
 }
--- texk/kpathsea/win32/mktexupdmain.c
+++ texk/kpathsea/win32/mktexupdmain.c
@@ -1,3 +1,21 @@
+/* mktexupdmain.c
+
+   Copyright 2000, 2015 Akira Kakuto.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this library; if not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <kpathsea/kpathsea.h>
 
 #include "mktexupd.h"
@@ -8,7 +26,7 @@ int main(int argc, char **argv)
   char file[256];
   char path[256];
   char *p;
-  int i;
+  size_t i;
 
   kpse_set_program_name (argv[0], NULL);
   if(argc != 3) {
@@ -18,8 +36,9 @@ int main(int argc, char **argv)
   strcpy(dir, argv[1]);
   strcpy(file, argv[2]);
   for(p = dir; *p; ++p) {
-    if(*p == '\\') *p = '/';
-    else if (IS_KANJI(p)) p++;
+    if(IS_KANJI(p))
+      p++;
+    else if(*p == '\\') *p = '/';
   }
   i = strlen(dir);
   while(dir[i-1] == '/')
