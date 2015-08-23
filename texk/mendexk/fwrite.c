4400cc1b43bda5153b77749785a4235e0b31071c
Takuji Tanaka <ttk@t-lab.opal.ne.jp>
2015-08-23 11:16:32 +0000

    texk/mendexk: Bug fix (segmentation fault) due to r.38182
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@38186 c570f23f-e606-0410-a88d-b1316a301751

--- texk/mendexk/fwrite.c
+++ texk/mendexk/fwrite.c
@@ -124,7 +124,7 @@ void indwrite(char *filename, struct index *ind, int pagenum)
 	FILE *fp;
 	int conv_euc_to_euc;
 
-	if (filename[0]!='\0' && kpse_out_name_ok(filename)) fp=fopen(filename,"wb");
+	if (filename && kpse_out_name_ok(filename)) fp=fopen(filename,"wb");
 	else {
 		fp=stdout;
 #ifdef WIN32
@@ -303,7 +303,7 @@ void indwrite(char *filename, struct index *ind, int pagenum)
 	}
 	fputs(postamble,fp);
 
-	if (filename[0]!='\0') fclose(fp);
+	if (filename) fclose(fp);
 }
 
 /*   write page block   */
