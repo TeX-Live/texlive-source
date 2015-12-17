ecbce881abeaa5dfbf6b9b2965d9875a24eb8bb6
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2015-12-17 07:20:42 +0000

    Import m-tx 0.61
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@39128 c570f23f-e606-0410-a88d-b1316a301751

---
+++ utils/m-tx/mtx-src/ChangeLog.C
@@ -0,0 +1,4 @@
+2012-05-16  Peter Breitenlohner  <peb@mppmu.mpg.de>
+        
+        * patch-01-write-bin (new): Use binary mode for output files.
+        From Akira Kakuto <kakuto@fuk.kindai.ac.jp> (W32TeX).
