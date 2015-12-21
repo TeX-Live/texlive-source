e907283120bee14044120e032bd1d8f2fe998129
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2015-12-21 06:41:40 +0000

    web2c/lulatexdir: sync with the upstream
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@39160 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/luatexdir/luatex.c
+++ texk/web2c/luatexdir/luatex.c
@@ -29,9 +29,9 @@
 #define TeX
 
 int luatex_version = 87;        /* \.{\\luatexversion}  */
-int luatex_revision = '0';      /* \.{\\luatexrevision}  */
-int luatex_date_info = 2015121700;     /* the compile date is now hardwired */
-const char *luatex_version_string = "beta-0.87.0";
+int luatex_revision = '1';      /* \.{\\luatexrevision}  */
+int luatex_date_info = 2015122000;     /* the compile date is now hardwired */
+const char *luatex_version_string = "beta-0.87.1";
 const char *engine_name = my_name;     /* the name of this engine */
 
 #include <kpathsea/c-ctype.h>
