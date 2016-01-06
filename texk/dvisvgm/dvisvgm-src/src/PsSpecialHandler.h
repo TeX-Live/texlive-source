70aed90d4dbee13eb3d91e3fdfa5cbcce9904095
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2016-01-06 05:27:06 +0000

    texk/dvisvgm: import dvisvgm 1.14.1
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@39295 c570f23f-e606-0410-a88d-b1316a301751

--- texk/dvisvgm/dvisvgm-src/src/PsSpecialHandler.h
+++ texk/dvisvgm/dvisvgm-src/src/PsSpecialHandler.h
@@ -1,8 +1,8 @@
 /*************************************************************************
 ** PsSpecialHandler.h                                                   **
 **                                                                      **
-** This file is part of dvisvgm -- the DVI to SVG converter             **
-** Copyright (C) 2005-2015 Martin Gieseking <martin.gieseking@uos.de>   **
+** This file is part of dvisvgm -- a fast DVI to SVG converter          **
+** Copyright (C) 2005-2016 Martin Gieseking <martin.gieseking@uos.de>   **
 **                                                                      **
 ** This program is free software; you can redistribute it and/or        **
 ** modify it under the terms of the GNU General Public License as       **
@@ -89,6 +89,7 @@ class PsSpecialHandler : public SpecialHandler, public DVIEndPageListener, prote
 
 	protected:
 		void initialize ();
+		void initgraphics ();
 		void moveToDVIPos ();
 		void executeAndSync (std::istream &is, bool updatePos);
 		void processHeaderFile (const char *fname);
