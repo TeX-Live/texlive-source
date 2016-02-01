8d254afa4f139b52f30dc0fe6591aec3501571e9
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2016-02-01 02:55:25 +0000

    texk/ptexenc, web2c/eptexdir, web2c/euptexdir: add a new primitive \epTeXinputencoding. (from N. Abe).
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@39532 c570f23f-e606-0410-a88d-b1316a301751

--- texk/ptexenc/ptexenc.c
+++ texk/ptexenc/ptexenc.c
@@ -840,6 +840,14 @@ long input_line2(FILE *fp, unsigned char *buff, long pos,
     return last;
 }
 
+boolean setinfileenc(FILE *fp, const char *str){
+    int enc;
+    enc = string_to_enc(str);
+    if (enc < 0) return false;
+    infile_enc[fileno(fp)] = enc;
+    return true;
+}
+
 #ifdef WIN32
 void clear_infile_enc(FILE *fp)
 {
