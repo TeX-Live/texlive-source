dd0341af92fba20f4d69ac52367278a11d0a5237
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2016-05-04 07:12:47 +0000

    dvipdfm-x: improve support of SOURCE_DATE_EPOCH
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@40883 c570f23f-e606-0410-a88d-b1316a301751

--- texk/dvipdfm-x/pdffont.c
+++ texk/dvipdfm-x/pdffont.c
@@ -90,10 +90,9 @@ pdf_font_set_dpi (int font_dpi)
 
 /* If an environment variable SOURCE_DATE_EPOCH is correctly defined like
  * SOURCE_DATE_EPOCH=1456304492, then returns this value, to be used as the
- * 'current time', otherwise returns INVALID_EPOCH_VALUE (= 0xfffffffffULL).
- * Here we follow the limit in Microsoft Visual Studio 2010, that is,
- * the value should be less than 32535291600, so we define 0xfffffffffULL
- * as INVALID_EPOCH_VALUE.
+ * 'current time', otherwise returns INVALID_EPOCH_VALUE (= 0x7fffffffffffffffULL).
+ * In the case of Microsoft Visual Studio 2010, the value should be less
+ * than 32535291600.
  */
 
 time_t
