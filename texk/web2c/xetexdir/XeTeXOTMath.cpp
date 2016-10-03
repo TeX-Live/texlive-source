4ffca61cb2c3f985e88d713cc6618ff9ae82715d
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2016-10-03 11:46:26 +0000

    xetexdir/XeTeXOTMath.cpp: sync with the upstream
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@42194 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/xetexdir/XeTeXOTMath.cpp
+++ texk/web2c/xetexdir/XeTeXOTMath.cpp
@@ -34,6 +34,7 @@ authorization from the copyright holders.
 #include <w2c/config.h>
 
 #include <assert.h>
+#include <algorithm>
 
 #include "XeTeXOTMath.h"
 
@@ -150,8 +151,13 @@ get_native_mathsy_param(int f, int n)
 {
     int rval = 0;
 
-    if (n == math_quad || n == delim2)
+    if (n == math_quad) {
         rval = fontsize[f];
+    }
+    else if (n == delim2) { // XXX not sure what OT parameter we should use here;
+                            // for now we use 1.5em, clamped to delim1 height
+        rval = std::min<int>(1.5 * fontsize[f], get_native_mathsy_param(f, delim1));
+    }
     else {
         if (n < sizeof(TeX_sym_to_OT_map) / sizeof(mathConstantIndex)) {
             mathConstantIndex ot_index = TeX_sym_to_OT_map[n];
