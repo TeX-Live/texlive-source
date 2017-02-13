b9f565714a1bf21f01739a6496c7e585bc04e731
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2017-02-13 14:08:26 +0000

    luatexdir: sync with the upstream
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@43207 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/luatexdir/luaffi/ffi.h
+++ texk/web2c/luatexdir/luaffi/ffi.h
@@ -42,7 +42,7 @@ extern "C" {
 #include <sys/mman.h>
 #endif
 
-#ifdef _WIN32 || _FreeBSD__ || __NetBSD__ || __OpenBSD__ || __bsdi__ || __DragonFly__
+#if ( defined( _WIN32) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__bsdi__) || defined(__DragonFly__))
 /* We should include something equivalent to */
 /* complex.h                                 */
 #else
