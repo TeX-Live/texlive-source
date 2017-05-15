5a724ca15dbcd74cb7574ccef23bb1ca2e8f48b8
Karl Berry <karl@freefriends.org>
2017-05-15 21:14:35 +0000

    lwarp (15may17)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@44369 c570f23f-e606-0410-a88d-b1316a301751

--- texk/texlive/linked_scripts/lwarp/lwarpmk.lua
+++ texk/texlive/linked_scripts/lwarp/lwarpmk.lua
@@ -4,7 +4,7 @@
 
 -- Print the usage of the lwarpmk command:
 
-printversion = "v0.30"
+printversion = "v0.31"
 
 function printhelp ()
 print ("lwarpmk: Use lwarpmk -h or lwarpmk --help for help.") ;
