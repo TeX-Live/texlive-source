86fdc56e7d7a6a1c5b1c453cd83d4639a3b6de5a
Karl Berry <karl@freefriends.org>
2017-06-06 21:55:13 +0000

    texosquery (23may17)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@44509 c570f23f-e606-0410-a88d-b1316a301751

--- texk/texlive/linked_scripts/texosquery/texosquery-jre5.sh
+++ texk/texlive/linked_scripts/texosquery/texosquery-jre5.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
 
-jarpath=`kpsewhich --progname=texosquery --format=texmfscripts texosquery-jre5.jar`
+kernel=`uname -s`
+if test "${kernel#*CYGWIN}" != "$kernel"
+then
+ jarpath=`cygpath -w $(kpsewhich --progname=texosquery --format=texmfscripts texosquery-jre5.jar)`
+else
+ jarpath=`kpsewhich --progname=texosquery --format=texmfscripts texosquery-jre5.jar`
+fi
 java -jar "$jarpath" "$@"
