11c8ee42507d6cb92cc2728c65f3ae0f3c763d5b
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2017-06-20 22:35:36 +0000

    fix my mistakes in zziplib
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@44657 c570f23f-e606-0410-a88d-b1316a301751

--- libs/zziplib/zziplib-src/bins/unzzipcat-zip.c
+++ libs/zziplib/zziplib-src/bins/unzzipcat-zip.c
@@ -43,22 +43,6 @@ static void unzzip_cat_file(ZZIP_DIR* disk, char* name, FILE* out)
     }
 }
 
-#if !defined(HAVE_STRNDUP)
-static char *
-strndup(char *p, size_t maxlen)
-{
-  char *r;
-  if (!p)
-    return 0;
-  r = malloc(maxlen + 1);
-  if (!r)
-    return r;
-  strncpy(r, p, maxlen);
-  r[maxlen] = '\0';
-  return r;
-}
-#endif
-
 static FILE* create_fopen(char* name, char* mode, int subdirs)
 {
    if (subdirs)
