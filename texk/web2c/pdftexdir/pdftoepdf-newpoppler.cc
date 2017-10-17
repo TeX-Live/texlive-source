d99870f346d661bf6b6090feb1cb97bf9bdb1c5a
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2017-10-17 04:34:41 +0000

    pdftexdir: add new files to support poppler 0.59.0 or new ones (typo)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@45552 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/pdftexdir/pdftoepdf-newpoppler.cc
+++ texk/web2c/pdftexdir/pdftoepdf-newpoppler.cc
@@ -19,7 +19,8 @@ with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /*
 This is based on the patch texlive-poppler-0.59.patch <2017-09-19> at
-https://git.archlinux.org/svntogit/packages.git/plain/texlive-bin/trunkby Arch Linux. A little modifications are made to avoid a crash for
+https://git.archlinux.org/svntogit/packages.git/plain/texlive-bin/trunk
+by Arch Linux. A little modifications are made to avoid a crash for
 some kind of pdf images, such as figure_missing.pdf in gnuplot.
 The poppler should be 0.59.0 or newer versions.
 POPPLER_VERSION should be defined.
