bfbdaeb1de7ef75a655406b4917abceeb28d2b3b
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2018-11-23 09:42:40 +0000

    support non-ascii values for variables (w32 only)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@49229 c570f23f-e606-0410-a88d-b1316a301751

--- texk/kpathsea/elt-dirs.c
+++ texk/kpathsea/elt-dirs.c
@@ -1,7 +1,7 @@
 /* elt-dirs.c: Translate a path element to its corresponding director{y,ies}.
 
    Copyright 1993, 1994, 1995, 1996, 1997, 2008, 2009, 2010, 2011, 2016,
-   2017 Karl Berry.
+   2017, 2018 Karl Berry.
    Copyright 1997, 1998, 1999, 2000, 2005 Olaf Weber.
 
    This library is free software; you can redistribute it and/or
@@ -408,6 +408,23 @@ kpathsea_element_dirs (kpathsea kpse, string elt)
 {
   str_llist_type *ret;
   unsigned i;
+
+#ifdef _WIN32
+/*
+  Change encoding of a variable into kpse->File_system_codepage
+  to support non-ascii values for the variable.
+*/
+  if (kpse->File_system_codepage != kpse->Win32_codepage) {
+    char *tname;
+    wchar_t *wtname;
+    wtname = get_wstring_from_mbstring (kpse->Win32_codepage,
+                                        elt, wtname = NULL);
+    tname = get_mbstring_from_wstring (kpse->File_system_codepage,
+                                       wtname, tname = NULL);
+    elt = tname;
+    free(wtname);
+  }
+#endif
 
   /* If given nothing, return nothing.  */
   if (!elt || !*elt)
