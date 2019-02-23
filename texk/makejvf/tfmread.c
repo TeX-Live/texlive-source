8b54476257ed0c1cf84dbc11b0f04c2726e78c96
Hironobu Yamashita <h.y.acetaminophen@gmail.com>
2019-02-23 22:34:30 +0000

    makejvf: forbid invalid usages
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@50106 c570f23f-e606-0410-a88d-b1316a301751

--- texk/makejvf/tfmread.c
+++ texk/makejvf/tfmread.c
@@ -93,20 +93,17 @@ int jfmread(int kcode)
 
 int tfmget(char *name)
 {
-	char nbuff[1024];
+	char *nbuff;
 	FILE *fp;
 
-	if (strlen(name) >= 1020) { /* <buffer size> - ".tfm" */
-		fprintf(stderr,"Too long input file name.\n");
-		exit(1);
-	}
+	nbuff = xmalloc(strlen(name)+4);
 	strcpy(nbuff,name);
+	strcat(nbuff,".tfm");
 	fp = fopen(nbuff,"rb");
 	if (fp == NULL) {
-		strcat(nbuff,".tfm");
-		fp = fopen(nbuff,"rb");
+		fp = fopen(name,"rb"); /* just in case ... */
 		if (fp == NULL) {
-			fprintf(stderr,"%s is not found.\n",name);
+			fprintf(stderr,"Cannot open %s for input.\n",nbuff);
 			exit(1);
 		}
 	}
