b749dc87678d336cd8d80b24123e07bbe5f5d0db
Karl Berry <karl@freefriends.org>
2019-09-28 21:33:38 +0000

    glossaries (28sep19)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@52199 c570f23f-e606-0410-a88d-b1316a301751

--- texk/texlive/linked_scripts/texlive/tlmgr.pl
+++ texk/texlive/linked_scripts/texlive/tlmgr.pl
@@ -1095,10 +1095,9 @@ sub backup_and_remove_package {
   }
   if ($opts{"backup"}) {
     $tlp->make_container($::progs{'compressor'}, $localtlpdb->root,
-                         destdir => $opts{"backupdir"}, 
-                         containername => "${pkg}.r" . $tlp->revision,
-                         relative => $tlp->relocated,
-                         user => 1);
+                         $opts{"backupdir"}, 
+                         "${pkg}.r" . $tlp->revision,
+                         $tlp->relocated);
     if ($autobackup) {
       # in case we do auto backups we remove older backups
       clear_old_backups($pkg, $opts{"backupdir"}, $autobackup);
@@ -2165,9 +2164,7 @@ sub action_backup {
         $tlp->revision . ".tar.$compressorextension\n");
       if (!$opts{"dry-run"}) {
         $tlp->make_container($::progs{'compressor'}, $localtlpdb->root,
-                             destdir => $opts{"backupdir"},
-                             containername => "${pkg}.r" . $tlp->revision,
-                             user => 1);
+                             $opts{"backupdir"}, "${pkg}.r" . $tlp->revision);
       }
     }
   }
@@ -2282,13 +2279,8 @@ sub write_w32_updater {
     push (@rst_tlpobj, "tlpkg\\tlpobj\\$pkg.tlpobj");
     push (@rst_info, "$pkg ^($oldrev^)");
     next if ($opts{"dry-run"});
-    # create backup;
-    # make_container expects filename with format: somename.r[0-9]+
-    my ($size, undef, $fullname)
-      = $localtlp->make_container("tar", $root,
-                                  destdir => $temp,
-                                  containername => "__BACKUP_$pkg.r$oldrev",
-                                  user => 1);
+    # create backup; make_container expects file name in a format: some-name.r[0-9]+
+    my ($size, undef, $fullname) = $localtlp->make_container("tar", $root, $temp, "__BACKUP_$pkg.r$oldrev");
     if ($size <= 0) {
       tlwarn("$prg: Creation of backup container of $pkg failed.\n");
       return 1; # backup failed? abort
@@ -3208,10 +3200,8 @@ sub action_update {
       if ($opts{"backup"} && !$opts{"dry-run"}) {
         my $compressorextension = $Compressors{$::progs{'compressor'}}{'extension'};
         $tlp->make_container($::progs{'compressor'}, $root,
-                             destdir => $opts{"backupdir"},
-                             containername => "${pkg}.r" . $tlp->revision,
-                             relative => $tlp->relocated,
-                             user => 1);
+                             $opts{"backupdir"}, "${pkg}.r" . $tlp->revision,
+                             $tlp->relocated);
         $unwind_package =
             "$opts{'backupdir'}/${pkg}.r" . $tlp->revision . ".tar.$compressorextension";
         
@@ -3249,12 +3239,9 @@ sub action_update {
         # no backup was made, so let us create a temporary .tar file
         # of the package
         my $tlp = $localtlpdb->get_package($pkg);
-        my ($s, undef, $fullname)
-          = $tlp->make_container("tar", $root,
-                                 destdir => $temp,
-                         containername => "__BACKUP_${pkg}.r" . $tlp->revision,
-                                 relative => $tlp->relocated,
-                                 user => 1);
+        my ($s, undef, $fullname) = $tlp->make_container("tar", $root, $temp,
+                                      "__BACKUP_${pkg}.r" . $tlp->revision,
+                                      $tlp->relocated);
         if ($s <= 0) {
           tlwarn("\n$prg: Creation of backup container of $pkg failed.\n");
           tlwarn("$prg: Continuing to update other packages, please retry...\n");
