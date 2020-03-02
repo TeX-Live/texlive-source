3abb35a515b56ae6a9a732e61d560d5128cf33cb
Karl Berry <karl@freefriends.org>
2020-03-02 21:27:21 +0000

    off by one
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@54013 c570f23f-e606-0410-a88d-b1316a301751

--- texk/xdvik/main.c
+++ texk/xdvik/main.c
@@ -457,7 +457,7 @@ is_good_dvi_file(const char *filename, Boolean from_history)
 	/* REALPATH failed, use real_filename */
 	size_t n = strlen(real_filename);
 	if (n >= MAXPATHLEN) {
-	  n = MAXPATHLEN;
+	  n = MAXPATHLEN - 1;
 	}
 
 	memcpy(canonical_path, real_filename, n);
