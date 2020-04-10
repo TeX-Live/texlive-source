9446ca809f7478b0064194502a717121db4d8846
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2020-04-10 04:02:01 +0000

    support \special{psfile=non-ascii-name} in dvips
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@54638 c570f23f-e606-0410-a88d-b1316a301751

--- texk/dvipsk/dospecial.c
+++ texk/dvipsk/dospecial.c
@@ -286,7 +286,7 @@ GetKeyVal(char *str, int *tno) /* returns NULL if none found, else next scan poi
      /* str : starting point for scan */
      /* tno : table entry number of keyword, or -1 if keyword not found */
 {
-   register char *s;
+   unsigned char *s;
    register int i;
    register char t;
 
