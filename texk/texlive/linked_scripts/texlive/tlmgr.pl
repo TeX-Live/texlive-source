7815577efacc56482db2d627980882ef642ebfdf
Karl Berry <karl@freefriends.org>
2023-03-08 21:31:49 +0000

    l3build (8mar23)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@66471 c570f23f-e606-0410-a88d-b1316a301751

--- texk/texlive/linked_scripts/texlive/tlmgr.pl
+++ texk/texlive/linked_scripts/texlive/tlmgr.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# $Id: tlmgr.pl 66378 2023-03-05 21:09:54Z karl $
+# $Id: tlmgr.pl 66457 2023-03-07 23:07:12Z preining $
 # Copyright 2008-2023 Norbert Preining
 # This file is licensed under the GNU General Public License version 2
 # or any later version.
@@ -8,8 +8,8 @@
 
 use strict; use warnings;
 
-my $svnrev = '$Revision: 66378 $';
-my $datrev = '$Date: 2023-03-05 22:09:54 +0100 (Sun, 05 Mar 2023) $';
+my $svnrev = '$Revision: 66457 $';
+my $datrev = '$Date: 2023-03-08 00:07:12 +0100 (Wed, 08 Mar 2023) $';
 my $tlmgrrevision;
 my $tlmgrversion;
 my $prg;
@@ -896,7 +896,7 @@ sub handle_execute_actions {
     my $lmtx = "$bindir/luametatex";
     if (defined($localtlpdb->get_package('context'))
         && (-x "$lmtx" || -x "$lmtx.exe")
-        && (wndws() || TeXLive::TLUtils::system_ok("$lmtx --version"))
+        && TeXLive::TLUtils::system_ok("$lmtx --version")
        ) {
       $errors += do_cmd_and_check("mtxrun --generate");
       $errors += do_cmd_and_check("context --luatex --generate");
@@ -10254,7 +10254,7 @@ This script and its documentation were written for the TeX Live
 distribution (L<https://tug.org/texlive>) and both are licensed under the
 GNU General Public License Version 2 or later.
 
-$Id: tlmgr.pl 66378 2023-03-05 21:09:54Z karl $
+$Id: tlmgr.pl 66457 2023-03-07 23:07:12Z preining $
 =cut
 
 # test HTML version: pod2html --cachedir=/tmp tlmgr.pl >/tmp/tlmgr.html
