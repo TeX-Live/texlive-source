5abcb65895bd723b458b80d6885b383196c89be2
Karl Berry <karl@freefriends.org>
2023-12-12 21:08:32 +0000

    l3build (12dec23)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@69100 c570f23f-e606-0410-a88d-b1316a301751

--- texk/texlive/linked_scripts/l3build/l3build.lua
+++ texk/texlive/linked_scripts/l3build/l3build.lua
@@ -1,5 +1,3 @@
-#!/usr/bin/env texlua
-
 --[[
 
 File l3build.lua Copyright (C) 2014-2022 The LaTeX Project
@@ -25,7 +23,7 @@ for those people who are interested.
 --]]
 
 -- Version information
-release_date = "2023-11-01"
+release_date = "2023-12-12"
 
 -- File operations are aided by the LuaFileSystem module
 local lfs = require("lfs")
@@ -165,16 +163,7 @@ if #checkconfigs > 1 then
     end
     if next(failed) then
       for _,config in ipairs(failed) do
-        print("Failed tests for configuration \"" .. config .. "\":")
-        print("\n  Check failed with difference files")
-        local testdir = testdir
-        if config ~= "build" then
-          testdir = testdir .. "-" .. config
-        end
-        for _,i in ipairs(ordered_filelist(testdir,"*" .. os_diffext)) do
-          print("  - " .. testdir .. "/" .. i)
-        end
-        print("")
+        checkdiff(config)
       end
       if options["show-saves"] then
         local savecmds, recheckcmds = "", ""
@@ -200,8 +189,8 @@ if #checkconfigs > 1 then
         end
         print"To regenerate the test files, run\n"
         print(savecmds)
-        if recheckcmds ~= "" then
-          print"To detect engine specific differences, run after that\n"
+        if recheckcmds ~= "" and #checkengines ~= 1 then
+          print"To detect engine-specific differences, run after that\n"
           print(recheckcmds)
         end
       end
