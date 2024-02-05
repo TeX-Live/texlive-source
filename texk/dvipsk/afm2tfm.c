aeffe3745cd73e7853b42e4ffe6537aa4f2e77e5
Karl Berry <karl@freefriends.org>
2024-02-05 17:23:27 +0000

    version/NEWS preparing for 2024
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@69711 c570f23f-e606-0410-a88d-b1316a301751

--- texk/dvipsk/afm2tfm.c
+++ texk/dvipsk/afm2tfm.c
@@ -1562,7 +1562,7 @@ writevpl(void)
 static void
 version(FILE *f)
 {
-  fputs ("afm2tfm(k) (dvips(k) 2023.1) 8.4\n", f);
+  fputs ("afm2tfm(k) (dvips(k) 2024.1) 8.4\n", f);
   fprintf (f, "%s\n", kpathsea_version_string);
   fputs ("Copyright 2023 Radical Eye Software.\n\
 There is NO warranty.  You may redistribute this software\n\
