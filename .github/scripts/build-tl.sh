58cc75d148b7990b5fadbad6f3467830af40efef
Norbert Preining <preining@logic.at>
2024-03-01 03:45:26 +0000

    [GHA] fix interactive mode
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@70278 c570f23f-e606-0410-a88d-b1316a301751

--- .github/scripts/build-tl.sh
+++ .github/scripts/build-tl.sh
@@ -127,7 +127,7 @@ mv inst/bin/* $arch
 case $buildsys in 
    ubuntu|debian)
      export DEBIAN_FRONTEND=noninteractive
-     apt-get install build-essential pkg-config libeigen3-dev libcurl4-openssl-dev libreadline-dev libboost-filesystem-dev libtirpc-dev flex libglu1-mesa-dev freeglut3-dev libosmesa6-dev libreadline6-dev zlib1g-dev bison libglm-dev libncurses-dev
+     apt-get install -y --no-install-recommends build-essential pkg-config libeigen3-dev libcurl4-openssl-dev libreadline-dev libboost-filesystem-dev libtirpc-dev flex libglu1-mesa-dev freeglut3-dev libosmesa6-dev libreadline6-dev zlib1g-dev bison libglm-dev libncurses-dev
      cd utils/asymptote
      ./configure --prefix=/tmp/asyinst --enable-static --enable-texlive-build CXXFLAGS=-std=c++11 --disable-gsl --disable-fftw --disable-lsp
      sed -i -e 's/^LIBS = /LIBS = -static-libgcc -static-libstdc++ /' Makefile
