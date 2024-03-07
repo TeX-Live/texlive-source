421717180561e28f8f1104d40fb91cef63f259a3
Norbert Preining <preining@logic.at>
2024-03-07 00:01:51 +0000

    [GHA asy] try statically linking libgcc and stdc++
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@70456 c570f23f-e606-0410-a88d-b1316a301751

--- .github/scripts/build-asy.sh
+++ .github/scripts/build-asy.sh
@@ -80,6 +80,8 @@ $TL_MAKE -j2
 
 # relink with static libtirpc if possible
 sed -i -e '/^LFLAGS/s/-ltirpc/-Wl,-Bstatic -ltirpc -Wl,-Bdynamic/' Makefile
+# static linking of stdc++
+sed -i -e '/^LDFLAG/s/= /= -static-libgcc -static-libstdc++ /' Makefile
 $TL_MAKE
 
 strip asy
