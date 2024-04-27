a430f4dd60a563c1e168b0c259a7be39233d92d1
Takuji Tanaka <ttk@t-lab.opal.ne.jp>
2024-04-27 13:01:15 +0000

    dvipdfm-x:  Support upTeX new encoding for combining characters via virtual fonts
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@71094 c570f23f-e606-0410-a88d-b1316a301751

---
+++ texk/dvipdfm-x/tests/upjv.pl
@@ -0,0 +1,134 @@
+(COMMENT THIS IS A KANJI FORMAT FILE)
+(DIRECTION TATE)
+(FAMILY UPJIS KANJI)
+(FACE F MRR)
+(CODINGSCHEME TEX KANJI TEXT)
+(DESIGNSIZE R 10.0)
+(COMMENT DESIGNSIZE IS IN POINTS)
+(COMMENT OTHER SIZES ARE MULTIPLES OF DESIGNSIZE)
+(CHECKSUM O 0)
+(FONTDIMEN
+   (SLANT R 0.0)
+   (SPACE R 0.0)
+   (STRETCH R 0.1)
+   (SHRINK R 0.0)
+   (XHEIGHT R 1.0)
+   (QUAD R 1.0)
+   (EXTRASPACE R 0.25)
+   (EXTRASTRETCH R 0.2)
+   (EXTRASHRINK R 0.125)
+   )
+(GLUEKERN
+   (LABEL O 0)
+   (GLUE O 1 R 0.5 R 0.0 R 0.5)
+   (GLUE O 3 R 0.25 R 0.0 R 0.25)
+   (STOP)
+   (LABEL O 1)
+   (GLUE O 3 R 0.25 R 0.0 R 0.25)
+   (STOP)
+   (LABEL O 2)
+   (GLUE O 0 R 0.5 R 0.0 R 0.5)
+   (GLUE O 1 R 0.5 R 0.0 R 0.5)
+   (GLUE O 3 R 0.25 R 0.0 R 0.25)
+   (GLUE O 5 R 0.5 R 0.0 R 0.5)
+   (STOP)
+   (LABEL O 3)
+   (GLUE O 0 R 0.25 R 0.0 R 0.25)
+   (GLUE O 1 R 0.25 R 0.0 R 0.25)
+   (GLUE O 2 R 0.25 R 0.0 R 0.25)
+   (GLUE O 3 R 0.5 R 0.0 R 0.25)
+   (GLUE O 4 R 0.25 R 0.0 R 0.25)
+   (GLUE O 5 R 0.25 R 0.0 R 0.25)
+   (STOP)
+   (LABEL O 4)
+   (GLUE O 0 R 0.5 R 0.0 R 0.0)
+   (GLUE O 1 R 0.5 R 0.0 R 0.0)
+   (GLUE O 3 R 0.75 R 0.0 R 0.25)
+   (GLUE O 5 R 0.5 R 0.0 R 0.0)
+   (STOP)
+   (LABEL O 5)
+   (GLUE O 1 R 0.5 R 0.0 R 0.5)
+   (GLUE O 3 R 0.25 R 0.0 R 0.25)
+   (KRN O 5 R 0.0)
+   (STOP)
+   )
+(CHARSINTYPE O 1
+   « ‘ “ 〈 〈 《 「 『 【 〔
+   〖 〘 〚 〝 （ ［ ｛ ｟
+   )
+(CHARSINTYPE O 2
+   » ’ ” 〉 、 〉 》 」 』 】
+   〕 〗 〙 〛 〞 〟 ） ， ］ ｝
+   ｠
+   )
+(CHARSINTYPE O 3
+   · ・ ： ；
+   )
+(CHARSINTYPE O 4
+   。 ．
+   )
+(CHARSINTYPE O 5
+   — ― ‥ …
+   )
+(TYPE O 0
+   (CHARWD R 1.0)
+   (CHARHT R 0.5)
+   (CHARDP R 0.5)
+   (COMMENT
+      (GLUE O 1 R 0.5 R 0.0 R 0.5)
+      (GLUE O 3 R 0.25 R 0.0 R 0.25)
+      )
+   )
+(TYPE O 1
+   (CHARWD R 0.5)
+   (CHARHT R 0.5)
+   (CHARDP R 0.5)
+   (COMMENT
+      (GLUE O 3 R 0.25 R 0.0 R 0.25)
+      )
+   )
+(TYPE O 2
+   (CHARWD R 0.5)
+   (CHARHT R 0.5)
+   (CHARDP R 0.5)
+   (COMMENT
+      (GLUE O 0 R 0.5 R 0.0 R 0.5)
+      (GLUE O 1 R 0.5 R 0.0 R 0.5)
+      (GLUE O 3 R 0.25 R 0.0 R 0.25)
+      (GLUE O 5 R 0.5 R 0.0 R 0.5)
+      )
+   )
+(TYPE O 3
+   (CHARWD R 0.5)
+   (CHARHT R 0.5)
+   (CHARDP R 0.5)
+   (COMMENT
+      (GLUE O 0 R 0.25 R 0.0 R 0.25)
+      (GLUE O 1 R 0.25 R 0.0 R 0.25)
+      (GLUE O 2 R 0.25 R 0.0 R 0.25)
+      (GLUE O 3 R 0.5 R 0.0 R 0.25)
+      (GLUE O 4 R 0.25 R 0.0 R 0.25)
+      (GLUE O 5 R 0.25 R 0.0 R 0.25)
+      )
+   )
+(TYPE O 4
+   (CHARWD R 0.5)
+   (CHARHT R 0.5)
+   (CHARDP R 0.5)
+   (COMMENT
+      (GLUE O 0 R 0.5 R 0.0 R 0.0)
+      (GLUE O 1 R 0.5 R 0.0 R 0.0)
+      (GLUE O 3 R 0.75 R 0.0 R 0.25)
+      (GLUE O 5 R 0.5 R 0.0 R 0.0)
+      )
+   )
+(TYPE O 5
+   (CHARWD R 1.0)
+   (CHARHT R 0.5)
+   (CHARDP R 0.5)
+   (COMMENT
+      (GLUE O 1 R 0.5 R 0.0 R 0.5)
+      (GLUE O 3 R 0.25 R 0.0 R 0.25)
+      (KRN O 5 R 0.0)
+      )
+   )
