e0493c62d0bf84e17f3636f1a17db59204612e1d
Norbert Preining <preining@logic.at>
2025-02-09 07:39:47 +0000

    Simplify asy build scripts
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@73800 c570f23f-e606-0410-a88d-b1316a301751

--- .github/scripts/build-asy.sh
+++ .github/scripts/build-asy.sh
@@ -30,7 +30,7 @@ then
        export LANG=C.UTF-8
        export LC_ALL=C.UTF-8
        apt-get update -q -y
-       apt-get install -y --no-install-recommends bash gcc g++ make perl libfontconfig-dev libx11-dev libxmu-dev libxaw7-dev build-essential
+       apt-get install -y --no-install-recommends bash gcc g++ make perl libfontconfig-dev libx11-dev libxmu-dev libxaw7-dev build-essential libtool-bin
        apt-get install -y --no-install-recommends build-essential pkg-config libeigen3-dev libcurl4-openssl-dev libreadline-dev libboost-filesystem-dev flex libglu1-mesa-dev freeglut3-dev libosmesa6-dev libreadline6-dev zlib1g-dev bison libglm-dev libncurses-dev python3 libtirpc-dev
        ;;
      freebsd)
@@ -62,8 +62,8 @@ case "$arch" in
     export TL_MAKE=gmake
     export CC=gcc
     export CXX=g++
-    export CFLAGS=-D_NETBSD_SOURCE
-    export CXXFLAGS='-D_NETBSD_SOURCE -std=c++11'
+    #export CFLAGS=-D_NETBSD_SOURCE
+    #export CXXFLAGS='-D_NETBSD_SOURCE -std=c++11'
     ;;
 esac
 
@@ -74,17 +74,10 @@ touch ./utils/asymptote/camp.tab.h
 touch ./utils/asymptote/GUI/pyUIClass/*
 
 cd utils/asymptote
-./configure --prefix=/tmp/asyinst --enable-static --enable-texlive-build CXXFLAGS=-std=c++11 \
+./configure --prefix=/tmp/asyinst --enable-static --enable-texlive-build \
 	--disable-gsl --disable-fftw --disable-lsp --disable-curl
+	LDFLAGS="-static-libgcc -static-libstdc++"
 $TL_MAKE -j2
-
-# relink with static libtirpc if possible
-sed -i -e '/^LFLAGS/s/-ltirpc/-Wl,-Bstatic -ltirpc -Wl,-Bdynamic/' Makefile
-
-# as of 2.88 necessary
-# static linking of stdc++
-sed -i -e '/^LFLAG/s/= /= -static-libgcc -static-libstdc++ /' Makefile
-$TL_MAKE
 
 strip asy
 
