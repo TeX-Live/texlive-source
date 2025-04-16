47a276c5261dc1a3f2af36fe97f47e4dfe01abfb
Andreas Scherer <andreas_tex@freenet.de>
2025-04-16 06:35:40 +0000

    Administrivia.
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@74960 c570f23f-e606-0410-a88d-b1316a301751

--- texk/ps2pk/spaces.h
+++ texk/ps2pk/spaces.h
@@ -79,25 +79,6 @@ struct XYspace {
                              /* subroutine of iconvert                       */
        fractpel (*iyconvert)(int32_t,int32_t,int32_t,int32_t);
                              /* subroutine of iconvert                       */
-#if 0
-/* These were my attempts at the C23-required replacement declarations
-   with all argument types (I gave up at ixconvert and iyconvert). But
-   the result still gets errors about incompatible types, so leaving
-   them disabled. The [=...] are my attempts at listing the functions
-   that get assigned to these structure members. --karl, 8apr25  */
-       /* calculate "fractpoint" X,Y from float X,Y  [=FXYconvert]:  */
-       void (*convert)(struct fractpoint *,struct XYspace *,double,double);
-       /* calculate "fractpoint" X,Y from int X,Y    [=ForceFloat,IXYConvert]: */
-       void (*iconvert)(struct fractpoint *,struct XYspace *,long,long);
-       /* subroutine of convert  [=FXonly,FYonly,FXYboth] */
-       fractpel (*xconvert)(DOUBLE,DOUBLE,DOUBLE,DOUBLE);
-       /* subroutine of convert  [=same]                  */
-       fractpel (*yconvert)(DOUBLE,DOUBLE,DOUBLE,DOUBLE);
-       /* subroutine of iconvert  [=IXonly,IYonly,IXYboth] */
-       fractpel (*ixconvert)();
-       /* subroutine of iconvert  [=same] */
-       fractpel (*iyconvert)();
-#endif
        int ID;               /* unique identifier (used in font caching)     */
        unsigned char context;  /* device context of coordinate space         */
        struct doublematrix tofract;  /* xform to get to fractional pels      */
