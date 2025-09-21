59a2d2bfb10e79633a2e92863ce3cfba33ce8667
Karl Berry <karl@freefriends.org>
2025-09-21 21:26:28 +0000

    sleep and debug if output file nested-range.ind does not (yet?) exist
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@76374 c570f23f-e606-0410-a88d-b1316a301751

--- texk/makeindexk/tests/nested-range-test.pl
+++ texk/makeindexk/tests/nested-range-test.pl
@@ -11,14 +11,26 @@ require "$srcdir/../tests/common-test.pl";
 exit (&main ());
 
 sub main {
-  my $ret = &test_run ("./makeindex", "$srcdir/tests/nested-range.idx",
+  my @test_args = ("./makeindex", "$srcdir/tests/nested-range.idx",
                        "-o", "nested-range.ind",
                        "-t", "nested-range.ilg");
-
-  # The test fails if the output contains \(.
+  my $ret = &test_run (@test_args);
+  if ($ret != 0) {
+    warn "test_run failed ($ret): $! (cmd=@test_args)\n";
+    die `pwd; ls -lt`;
+  }
+  
   my $bad = 0;
   local *IND;
   $IND = "nested-range.ind";
+  
+  # Seems that unpredictably the output file doesn't exist yet?
+  if (! -e $IND) {
+    warn `pwd; ls -lt`;
+    sleep 1;
+  }
+  
+  # The test fails if the output contains \(.
   open (IND) || die "open($IND) failed: $!";
   while (<IND>) {
     $bad = 1 if /\\\(/;
