3678201321593a090f7ff26cda378ebfbf1675cd
Karl Berry <karl@freefriends.org>
2025-12-28 23:06:34 +0000

    show all processes when output file does not exist
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@77204 c570f23f-e606-0410-a88d-b1316a301751

--- texk/makeindexk/tests/nested-range-test.pl
+++ texk/makeindexk/tests/nested-range-test.pl
@@ -11,8 +11,10 @@ require "$srcdir/../tests/common-test.pl";
 exit (&main ());
 
 sub main {
+  my $IND = "nested-range.ind";
+
   my @test_args = ("./makeindex", "$srcdir/tests/nested-range.idx",
-                       "-o", "nested-range.ind",
+                       "-o", $IND,
                        "-t", "nested-range.ilg");
   my $ret = &test_run (@test_args);
   if ($ret != 0) {
@@ -21,18 +23,15 @@ sub main {
   }
   
   my $bad = 0;
-  local *IND;
-  $IND = "nested-range.ind";
   
   # Seems that unpredictably the output file doesn't exist yet?
   if (! -e $IND) {
-    warn "test_run succeeded, but $IND doesn't exist? Sleeping (cmd=@test_args)\n";
-    warn `pwd; ls -lt; ps auxww | grep make`;
-    sleep 2;
+    warn "test_run succeeded, but $IND doesn't exist? (cmd=@test_args)\n";
+    warn `pwd; ls -lt; /l/bin/proc`;
   }
   
   # The test fails if the output contains \(.
-  open (IND) || die "open($IND) failed: $!";
+  open (IND, $IND) || die "open($IND) failed: $!";
   while (<IND>) {
     $bad = 1 if /\\\(/;
   }
