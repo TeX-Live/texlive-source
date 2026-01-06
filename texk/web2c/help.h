261171fdbfbc571bb0be5124e34200ff3a904838
Andreas Scherer <andreas_tex@freenet.de>
2026-01-06 13:48:19 +0000

    Revert "[WEB] Add option '-c' for TWILL."
    This reverts commit 22a8a9403e930ec872604c4c38cd8640aa33c8ff.
    
    DEK's 'twimac.tex' is not ready for general use. If you want to format
    TEX.WEB or MF.WEB, you should use the generic 'twimac-tex.tex' and adapt
    the preambles of those WEB programs accordingly. Good luck!
    
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@77275 c570f23f-e606-0410-a88d-b1316a301751

--- texk/web2c/help.h
+++ texk/web2c/help.h
@@ -661,8 +661,7 @@ const_string TWILLHELP[] = {
     "  unless otherwise specified by OUTFILE;",
     "  in this case, '-' specifies a null CHANGEFILE.",
     "",
-    "-c          use original 'twimac.tex' macros",
-    "-x          omit end-index, section names, table of contents",
+    "-x          omit cross-reference information",
     "-help       display this help and exit",
     "-version    output version information and exit",
     NULL
