8892c061e203b03ccf39ef2607d3c21c0f3ca819
Sigitas Tolusis <sigitas@vtex.lt>
2026-02-04 19:04:41 +0000

    [GHA] try to fix xdvipsk build on solaris
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@77593 c570f23f-e606-0410-a88d-b1316a301751

--- .github/scripts/build-tl-xdvipsk.sh
+++ .github/scripts/build-tl-xdvipsk.sh
@@ -112,6 +112,7 @@ case "$arch" in
       export CC="gcc -m32"
       export CXX="g++ -m32"
       crle -c /var/ld/ld.config -l /opt/csw/lib:/lib:/usr/lib 
+      ln -s /opt/csw/lib/i386/libstdc++.so.6 /opt/csw/lib/i386/libstdc++.so
     else
       export CC="gcc -m64"
       export CXX="g++ -m64"
